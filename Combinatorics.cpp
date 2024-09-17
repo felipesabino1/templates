4f0c56e0de10e4e6e989bb34444fd5d7ca89681b
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2024-09-17 16:48:50 -0300

    Create Combinatorics.cpp

---
+++ Combinatorics.cpp
@@ -0,0 +1,43 @@
+struct Comb{
+    vector<ll> fato,fatoinv;
+
+    Comb(int n){
+        fato.resize(n+10,-1);
+        fatoinv.resize(n+10,-1);
+    }
+
+    void set_tam(int n){
+        fato.resize(n+10,-1);
+        fatoinv.resize(n+10,-1);
+    }
+
+    ll fexp(ll a, ll b){
+        ll ans=1;
+        while(b){
+            if(b&1) ans=ans*a%mod;
+            b>>=1;
+            a=a*a%mod;
+        }
+        return ans;
+    }
+
+    ll fat(ll x){
+        if(x <= 1) return 1;
+        if(fato[x] != -1) return fato[x];
+        return fato[x]=fat(x-1)*x%mod;
+    }
+
+    ll fatinv(ll x){
+        if(x <= 1) return 1;
+        if(fatoinv[x] != -1) return fatoinv[x];
+        return fatoinv[x]=fexp(fat(x),mod-2);
+    }
+
+    ll toma(ll n, ll k){
+        if(k > n) return 0;
+        ll ans=fat(n);
+        ans=ans*fatinv(k)%mod;
+        ans=ans*fatinv(n-k)%mod;
+        return ans;
+    }
+};
