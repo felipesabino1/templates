f0d24a3435647be3a564f7adeaf1505a90f44938
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2024-10-30 11:27:38 -0300

    Create Sparse Table.cpp

---
+++ Data Structure/Sparse Table.cpp
@@ -0,0 +1,46 @@
+/*
+    Indexado de 1
+
+    Alteracoes: 
+    
+    Funcao f() da tabela, valor off
+*/
+typedef long long int TT;
+struct SparseTable{
+    int n; // tamanho 
+    vector<vector<TT>> tab; // sparse table
+    int TETO; // Teto do log2(n)
+    const TT off = ;
+
+    SparseTable(int n, vector<TT> &a){
+        this->n = n;
+        int t=0;
+        int d=1;
+        while(d <= n){
+            t++;
+            d<<=1;
+        }
+        TETO = t;
+        // build da Sparse Table
+        for(int i=1; i<=n; i++) tab[i][0] = a[i];
+        for(int j=1; j<TETO; j++) for(int i=0; i+(1<<j)-1 <= n; i++) tab[i][j] = f(tab[i][j-1],tab[i+(1<<(j-1))][j-1]);
+    }
+
+    ~SparseTable(){}
+
+    // operacao da sparse table
+    TT f(TT x, TT y){
+        return ;
+    }
+
+    TT query(int l,int r){
+        TT ans = off;
+        for(int j=TETO-1; j>=0; j--){
+            if(l+(1<<j)-1 <= r){
+                ans = f(ans, tab[l][j]);
+                l +=(1<<j);
+            }
+        }
+        return ans;
+    }
+}
