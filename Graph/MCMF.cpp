88e88a6f634ccd25c5570ad8383cd2085edc6df3
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2024-10-31 17:16:20 -0300

    Create MCMF.cpp

---
+++ Graph/MCMF.cpp
@@ -0,0 +1,142 @@
+// tipo do fluxo, sempre tem que ser inteiro,
+// tipo do custo.
+template <class TT = int, class TT1 = ll>
+class MCMF {
+public:
+    struct Edge {
+        Edge(int a, TT b, TT1 c) : to(a), cap(b), cost(c) {}
+        int to;
+        TT cap;
+        TT1 cost;
+    };
+
+    MCMF(int size) {
+        n = size;
+        edges.resize(n);
+        pot.assign(n, 0);
+        dist.resize(n);
+        visit.assign(n, false);
+    }
+
+    pair<TT, TT1> mcmf(int src, int sink) {
+        pair<TT, TT1> ans(0, 0);
+        if(!SPFA(src, sink)) return ans;
+        fixPot();
+        // can use dijkstra to speed up depending on the graph
+        while(SPFA(src, sink)) {
+            auto flow = augment(src, sink);
+            ans.first += flow.first;
+            ans.second += flow.first * flow.second;
+            fixPot();
+        }
+        return ans;
+    }
+
+    void addEdge(int from, int to, TT cap, TT1 cost) {
+        edges[from].push_back(list.size());
+        list.push_back(Edge(to, cap, cost));
+        edges[to].push_back(list.size());
+        list.push_back(Edge(from, 0, -cost));
+    }
+private:
+    int n;
+    vector<vector<int>> edges;
+    vector<Edge> list;
+    vector<int> from;
+    vector<TT1> dist, pot;
+    vector<bool> visit;
+
+    /*bool dij(int src, int sink) {
+        TT1 INF = numeric_limits<TT1>::max();
+        dist.assign(n, INF);
+        from.assign(n, -1);
+        visit.assign(n, false);
+        dist[src] = 0;
+        for(int i = 0; i < n; i++) {
+            int best = -1;
+            for(int j = 0; j < n; j++) {
+                if(visit[j]) continue;
+                if(best == -1 || dist[best] > dist[j]) best = j;
+            }
+            if(dist[best] >= INF) break;
+            visit[best] = true;
+            for(auto e : edges[best]) {
+                auto ed = list[e];
+                if(ed.cap == 0) continue;
+                TT1 toDist = dist[best] + ed.cost + pot[best] - pot[ed.to];
+                assert(toDist >= dist[best]);
+                if(toDist < dist[ed.to]) {
+                    dist[ed.to] = toDist;
+                    from[ed.to] = e;
+                }
+            }
+        }
+        return dist[sink] < INF;
+    }*/
+
+    pair<TT, TT1> augment(int src, int sink) {
+        pair<TT, TT1> flow = {list[from[sink]].cap, 0};
+        for(int v = sink; v != src; v = list[from[v]^1].to) {
+            flow.first = min(flow.first, list[from[v]].cap);
+            flow.second += list[from[v]].cost;
+        }
+        for(int v = sink; v != src; v = list[from[v]^1].to) {
+            list[from[v]].cap -= flow.first;
+            list[from[v]^1].cap += flow.first;
+        }
+        return flow;
+    }
+
+    queue<int> q;
+    bool SPFA(int src, int sink) {
+        TT1 INF = numeric_limits<TT1>::max();
+        dist.assign(n, INF);
+        from.assign(n, -1);
+        q.push(src);
+        dist[src] = 0;
+        while(!q.empty()) {
+            int on = q.front();
+            q.pop();
+            visit[on] = false;
+            for(auto e : edges[on]) {
+                auto ed = list[e];
+                if(ed.cap == 0) continue;
+                TT1 toDist = dist[on] + ed.cost + pot[on] - pot[ed.to];
+                if(toDist < dist[ed.to]) {
+                    dist[ed.to] = toDist;
+                    from[ed.to] = e;
+                    if(!visit[ed.to]) {
+                        visit[ed.to] = true;
+                        q.push(ed.to);
+                    }
+                }
+            }
+        }
+        return dist[sink] < INF;
+    }
+
+    void fixPot() {
+        TT1 INF = numeric_limits<TT1>::max();
+        for(int i = 0; i < n; i++) {
+            if(dist[i] < INF) pot[i] += dist[i];
+        }
+    }
+};
+
+class Solution {
+public:
+
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        int n = robot.size();
+        int m = factory.size();
+        int src = n+m;
+        int sink = src+1;
+        MCMF mcmf(sink+1);
+        for(int i=0; i<n; i++){
+            mcmf.addEdge(src,i,1,0);
+            for(int j=0; j<m; j++) mcmf.addEdge(i,j+n,1,1,abs(factory[j][0]-robot[i]));
+        }
+        for(int j=0; j<m; j++) mcmf.addEdge(j+n,sink,factory[j][1],0);
+        return mcmf.mcmf(src,sink).second;
+    }
+};
