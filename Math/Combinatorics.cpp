f6dd0eafb6cbe3b873057bc6d7ed844915d73cc5
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-01-19 20:20:22 -0300

    Update Combinatorics.cpp

--- Math/Combinatorics.cpp
+++ Math/Combinatorics.cpp
@@ -1,6 +1,7 @@
+template <class TT = ll> 
 struct Comb{
-    vector<ll> fato,fatoinv;
-    vector<vector<ll>> tomatoma;
+    vector<TT> fato,fatoinv;
+    vector<vector<TT>> tomatoma;
 
     // setar so os fatorias e inversos
     Comb(int n){
@@ -26,14 +27,14 @@ struct Comb{
     }
 
     void set_tomatoma(int n,int m){
-        tomatoma.resize(n+10,vector<ll>(m+10));
+        tomatoma.resize(n+10,vector<TT>(m+10));
         for(int i=0; i<=m; i++) tomatoma[0][i]=0;
         for(int i=0; i<=n; i++) tomatoma[i][0]=1%mod;
         for(int i=1; i<=n; i++) for(int j=1; j<=m; j++) tomatoma[i][j] = (tomatoma[i-1][j-1] + tomatoma[i-1][j])%mod;
     }
 
-    ll fexp(ll a, ll b){
-        ll ans=1;
+    TT fexp(TT a, TT b){
+        TT ans=1;
         while(b){
             if(b&1) ans=ans*a%mod;
             b>>=1;
@@ -41,9 +42,21 @@ struct Comb{
         }
         return ans;
     }
+    void mult(vector<vector<TT>> &a, vector<vector<TT>> &b,vector<vector<TT>> & aux){
+        int n = a.size();
+        int m = a[0].size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                aux[i][j] = 0;
+                for(int k = 0; k< n; k++) aux[i][j] = (aux[i][j] + a[i][k]*a[k][j]%mod)%mod;
+            }
+        }
+        for(int i=0; i<n; i++) for(int j=0; j<m; j++) a[i][j] = aux[i][j];
+    }
 
-    ll fat(ll x){
-        if(ll(fato.size()) <= x){
+    // calcula e guarda as respostas dos fatoriais
+    TT fat(TT x){
+        if(TT(fato.size()) <= x){
             fato.resize(x+10,-1);
         }
         if(x <= 1) return 1;
@@ -51,8 +64,8 @@ struct Comb{
         return fato[x]=fat(x-1)*x%mod;
     }
 
-    ll fatinv(ll x){
-        if(ll(fatoinv.size()) <= x){
+    TT fatinv(TT x){
+        if(TT(fatoinv.size()) <= x){
             fatoinv.resize(x+10,-1);
         }
         if(x <= 1) return 1;
@@ -60,10 +73,10 @@ struct Comb{
         return fatoinv[x]=fexp(fat(x),mod-2);
     }
 
-    ll toma(ll n, ll k){
+    TT toma(TT n, TT k){
         if(n < 0) return 0;
         if(k > n) return 0;
-        ll ans;
+        TT ans;
         if(tomatoma.size() > n && tomatoma[0].size() > k){
             ans = tomatoma[n][k];
         }else{
@@ -74,7 +87,7 @@ struct Comb{
         return ans;
     }
 
-    ll inv(ll x){
+    TT inv(TT x){
         return fexp(x,mod-2);
     }
 };
