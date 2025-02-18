bd6eaddbb133c7548c7eb91b22676039b8e8a0a6
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-02-17 23:42:39 -0300

    Update Segment Tree.cpp

--- Data Structure/Segment Tree.cpp
+++ Data Structure/Segment Tree.cpp
@@ -38,7 +38,9 @@ struct Seg{
     vector<sono> lazy;
     // operacao de unir dois nos
     node merge(node x, node y){
-
+        if(x == off) return y;
+        if(y == off) return x;
+    
     }
 
     // coisar a lazy pra baixo
@@ -48,63 +50,63 @@ struct Seg{
 
         // atualizar as lazies dos filhos
 
-        lazy[u]=off_lazy;
+        lazy[u] = off_lazy;
     }
 
     // inicializar a seg
     void build(int u,int tl,int tr){
         if(tl == tr){
             // inicializar os caras bases
-            seg[u]={};
-            lazy[u]=off_lazy;
+            seg[u] = {};
+            lazy[u] = off_lazy;
             return;
         }
-        int tmid=tl+tr;
-        tmid>>=1;
-        build(lef(u),tl,tmid);
-        build(rig(u),tmid+1,tr);
-        seg[u]=merge(seg[lef(u)],seg[rig(u)]);
-        lazy[u]=off_lazy;
+        int tmid = tl + tr;
+        tmid >>= 1;
+        build(lef(u), tl, tmid);
+        build(rig(u), tmid+1, tr);
+        seg[u] = merge(seg[lef(u)], seg[rig(u)]);
+        lazy[u] = off_lazy;
     }
 
     // consulta em range
     node query_(int u,int tl,int tr,int l, int r){
         if(l > r) return off;
         if(tl == l && tr == r) return seg[u];
-        push(u,tl,tr);
-        int tmid=tl+tr;
-        tmid>>=1;
-        return merge(query_(lef(u),tl,tmid,l,min(tmid,r)),query_(rig(u),tmid+1,tr,max(tmid+1,l),r));
+        push(u, tl, tr);
+        int tmid= tl + tr;
+        tmid >>= 1;
+        return merge(query_(lef(u), tl, tmid, l, min(tmid,r)), query_(rig(u), tmid+1, tr, max(tmid+1,l), r));
     }
-    node query(int l,int r){
-        return query_(1,1,n,l,r);
+    node query(int l, int r){
+        return query_(1, 1, n, l, r);
     }
 
     // update em range
-    void update_(int u,int tl,int tr,int l,int r,TT x){
+    void update_(int u, int tl, int tr, int l, int r, TT x){
         if(l > r) return;
         if(tl == l && tr == r){
             // atualizar a seg e o lazy
             
             return;
         }
-        push(u,tl,tr);
-        int tmid=tl+tr;
-        tmid>>=1;
-        update_(lef(u),tl,tmid,l,min(tmid,r),x);
-        update_(rig(u),tmid+1,tr,max(tmid+1,l),r,x);
-        seg[u]=merge(seg[lef(u)],seg[rig(u)]);
+        push(u, tl, tr);
+        int tmid = tl + tr;
+        tmid >>= 1;
+        update_(lef(u), tl, tmid, l, min(tmid,r), x);
+        update_(rig(u), tmid+1, tr, max(tmid+1,l), r, x);
+        seg[u] = merge(seg[lef(u)], seg[rig(u)]);
     }
-    void update(int l,int r, TT x){
-        update_(1,1,n,l,r,x);
+    void update(int l, int r, TT x){
+        update_(1, 1, n, l, r, x);
     }
     
     // busca binaria pela esquerda a partir do ponto inicial x, pra satisfazer alguma funcao (indice mais a esquerda tal que somatorio <= v por exemplo)
     // retorna indice e os valores dos caras mais perto de x
-    pair<int,TT> bb_lef_(int u,int tl,int tr,int x, TT v){
-        int tmid = tl+tr;
-        tmid>>=1;
-        pair<int,TT> ret = make_pair(n+10,-1); /*Mudar segundo parametro de acordo com a operacao*/
+    pair<int,TT> bb_lef_(int u, int tl, int tr, int x, TT v){
+        int tmid = tl + tr;
+        tmid >>= 1;
+        pair<int,TT> ret = make_pair(n+10, -1); /*Mudar segundo parametro de acordo com a operacao*/
         if(tr <= x){
             // achei um segmento antes de x
             ret.second = v - seg[u].sum; /*Mudar de acordo com os atributos do node*/
