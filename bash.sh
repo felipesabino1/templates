c95561e9e99b88281475a973252bc31ebc95feaf
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-03-06 11:19:44 -0300

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+make brute
+make gen
+make at # codigo que muda
+
+while true; do
+    ./gen 2 > in
+    
+    start=$(date +%s)
+    ./at < in > out
+    end=$(date +%s)
+
+    echo "Elapsed Time in test $c: $(($end - $start)) seconds."
+    ./brute < in > aout
+    diff -B out aout > /dev/null || break
+    echo "Passou no caso $c."
+    cat out
+done
+
+echo "WA on the following case:"
+cat in
+echo "Your answer is:"
+cat out
+echo "Correct answer is:"
+cat aout
