b01e33ea0b5002294cb2dc02462a4f13a1453542
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-03-10 22:57:15 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lef(x) (x<<1)
+#define rig(x) (lef(x)|1)
+#define get_bit(x,i) (x&(1ll<<i))
+using ll = int64_t;
+using ull = uint64_t;
+using ld = long double;
+// const int MAX = 5e5+10;
+// const int mod = 998244353;
+// const int inf = 2'000'000'000;
+// const int TETO = 20;
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void test(){   
+  
+}
+
+int main (){
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    int ttt_ = 1;
+    cin >> ttt_;
+    while(ttt_--) test();
+    return 0;
+}
