70ca2caec216a62c09609363b4e664b1e5504d70
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-03-30 13:51:47 -0300

    Update Fractions.cpp

--- Math/Fractions.cpp
+++ Math/Fractions.cpp
@@ -2,22 +2,21 @@ template <class TT = ll>
 struct Frac{
     TT num,den; // o valor negativo da fracao fica so no numerador
  
-    Frac (TT num,TT den){
+    Frac (TT num_ = 0,TT den_ = 1) : num(num_), den(den_){
         TT g = __gcd(num,den);
         num/=g;
         den/=g;
-        if((num < 0 && den < 0) || (num > 0 && den > 0)){
-            num=abs(num);
-            den=abs(den);
-        }else if(num > 0){
-            num*=-1;
-            den*=-1;
-        }
-        this->num=num;
-        this->den=den;
+        fixSig(num,den);
     }
-    Frac(){}
-    ~Frac(){}
+    void fixSig(TT & num_, TT & den_){
+        if((num_ < 0 && den_ < 0) || (num_ > 0 && den_ > 0)){
+            num_=abs(num_);
+            den_=abs(den_);
+        }else if(num_ > 0){
+            num_*=-1;
+            den_*=-1;
+        }
+    }
 
     TT fexp(TT a, TT b){
         TT ans = 1;
@@ -40,11 +39,11 @@ struct Frac{
     }
 
     friend ostream &operator<<(ostream & os, const Frac & o){
-        os << "{" << o.num << "," << o.den << "}";
+        os << o.num << "/" << o.den;
         return os;
     }
  
-    bool const operator <(const Frac & o)const{
+    bool operator <(const Frac & o)const{
         if((den < 0 && o.den < 0) || (den > 0 && o.den > 0)) return num*o.den < o.num*den;
         return num*o.den > o.num*den; 
     }
@@ -57,23 +56,58 @@ struct Frac{
         return num != o.num || den != o.den;
     }
 
+    friend Frac &operator -(Frac & at){
+        at.num *= -1;
+        return at;
+    }
+
     void operator *=(const Frac & o){
         TT g1 = __gcd(num,o.den);
         TT g2 = __gcd(den,o.num);
         num = (num/g1)*(o.num/g2);
         den = (den/g2)*(o.den/g1);
+        fixSig(num,den);
+    }
+    void operator /=(const Frac & o){
+        TT g1 = __gcd(num,o.num);
+        TT g2 = __gcd(den,o.den);
+        num = (num/g1)*(o.den/g2);
+        den = (den/g2)*(o.num/g1);
+        fixSig(num,den);
+    }
+    void operator +=(const Frac & o){
+        TT lcm = den/__gcd(den,o.den)*o.den;
+        num = (lcm/den*num) + (lcm/o.den*o.num);
+        den = lcm;
+        fixSig(num,den);
+    }
+    void operator -=(const Frac & o){
+        TT lcm = den/__gcd(den,o.den)*o.den;
+        num = (lcm/den*num) + (lcm/o.den*o.num);
+        den = lcm;
+        fixSig(num,den);
     }
 
+    Frac operator *(const Frac & o){
+        TT g1 = __gcd(num,o.den);
+        TT g2 = __gcd(den,o.num);
+        return Frac((num/g1)*(o.num/g2), (den/g2)*(o.den/g1));
+    }
+    Frac operator /(const Frac & o){
+        TT g1 = __gcd(num,o.num);
+        TT g2 = __gcd(den,o.den);
+        return Frac((num/g1)*(o.den/g2), (den/g2)*(o.num/g1));
+    }
     Frac operator +(const Frac & o){
         TT lcm = den/__gcd(den,o.den)*o.den;
         return Frac((lcm/den*num) +(lcm/o.den*o.num),lcm);
     }
-
     Frac operator -(const Frac & o){
         TT lcm = den/__gcd(den,o.den)*o.den;
         return Frac((lcm/den*num) -(lcm/o.den*o.num),lcm);
     }
 
+    // exponenciacao
     void operator ^=(TT x){
         if(num == 0) return;
         if(x < 0){
