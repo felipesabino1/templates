3fd17039cae5cd80c5ade44928c8524057724389
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-03-31 17:26:01 -0300

    Update Convex Hull Trick.cpp

--- Data Structure/Convex Hull Trick.cpp
+++ Data Structure/Convex Hull Trick.cpp
@@ -1,23 +1,23 @@
 template <class TT = ll>
 struct Line{
     // coef angular, linear, criterio de comparacao
-    mutable T k,m,p;
+    mutable TT k,m,p;
     // aqui eu quero sempre deixar os com menor coef angular mais pra frente, conc pra baixo
     bool operator <(const Line & o) const{
         return k>o.k;
     }
-    bool operator <(const T o) const{
+    bool operator <(const TT o) const{
         return p<o;
     }
 };
 struct CHT : multiset<Line,less<>>{
-    static const T inf = std::numerical_limit<TT>::max();
-    T div(T a, T b){
+    static const TT inf = std::numerical_limit<TT>::max();
+    TT div(TT a, TT b){
         return a/b-((a^b) < 0 && a%b);
     }
  
     // x eh melhor que y?
-    bool insect(iterator x, iterator y){
+    bool isect(iterator x, iterator y){
         if(y == end()){
             x->p=inf;
             return false;
@@ -28,7 +28,7 @@ struct CHT : multiset<Line,less<>>{
             x->p=div(y->m-x->m,x->k-y->k);
         return x->p >= y->p;
     }
-    void add(T k, T m){
+    void add(TT k, TT m){
         auto z= insert({k,m,0});
         auto y=z++;
         auto x=y;
@@ -38,7 +38,7 @@ struct CHT : multiset<Line,less<>>{
         while((y=x) != begin() && (--x)->p >= y->p)
             insect(x,erase(y));
     }
-    T query(T x){
+    TT query(TT x){
         auto ans=lower_bound(x);
         return ans->k*x+ans->m;
     }
