5780e2a034adacdb749284788eaaa36835ee54e8
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-05-09 15:14:58 -0300

    Update Manacher.cpp

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -2,7 +2,7 @@
     Indexado de 0. O(N)
     
     Calcula a qtd de palindromos usando da propriedade que pal(i,j) = pal(i+1,j-1) + (s[i] == s[j])
-    Retorna man[i] = x tal que pal(i-x,i+x) eh palindromo (impar) ou pal(i-x,i+1+x) eh palindromo (par)
+    Retorna man[i] = x tal que x eh o tam da metade de um palindromo em que o i eh o meio, se for tamanho par, o i eh meio da esquerda, aba, no b eu conto 2
     man[0] = palindromos pares, man[1] = palindromos impares
 */
 vector<vector<int>> manacher(string & s){
@@ -12,14 +12,12 @@ vector<vector<int>> manacher(string & s){
     int l = -1, r = -1;
     for(int i=0; i<s.size(); i++){
         int qt = 0;
-        if(r >= i+1) qt = min(r - (i+1), man[0][l + (r-(i+1))]);
-        qt = max(0, qt);
+        if(r >= i+1) qt = min(r - (i+1)+1, man[0][l + (r-(i+1))]);
         while(i - qt >= 0 && i+1 + qt < n && s[i-qt] == s[i+1+qt]) qt++;
-        qt--;
         man[0][i] = qt;
-        if(r < i+1 + qt){
-            l = i - qt;
-            r = i+1 + qt;
+        if(r < i+1 + qt - 1){
+            l = i - qt + 1;
+            r = i+1 + qt - 1;
         }
     }
     // odd
@@ -28,11 +26,10 @@ vector<vector<int>> manacher(string & s){
         int qt = 0;
         if(r >= i) qt = min(r-i,man[1][l + (r-i)]);
         while(i-qt >= 0 && i+qt < n && s[i-qt] == s[i+qt]) qt++;
-        qt--;
         man[1][i] = qt;
-        if(r < i + qt){
-            l = i - qt;
-            r = i + qt;
+        if(r < i + qt - 1){
+            l = i - qt + 1;
+            r = i + qt - 1;
         }
     }
     return man;
