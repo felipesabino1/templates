b649d63b0134a271b427e1915b2b8e42e85657c1
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-05-22 14:58:10 -0300

    Create KMP.cpp

---
+++ String/KMP.cpp
@@ -0,0 +1,11 @@
+vector<int> kmp(string & s){
+    vector<int> p(s.size());
+    p[0] = 0;
+    for(int i=1; i<s.size(); i++){
+        int j = p[i-1];
+        while(j != 0 && s[i] != s[j]) j = p[j-1];
+        if(s[i] == s[j]) j++;
+        p[i] = j;
+    }
+    return p;
+}
