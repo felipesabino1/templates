220a94c889ffe9f154fe17e1c83f839067cb46a2
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-08-28 20:08:42 -0300

    Update Mod int.cpp

--- Math/Mod int.cpp
+++ Math/Mod int.cpp
@@ -1,29 +1,25 @@
-const int MOD = mod;
-
 struct mi {
     int v;
     explicit operator int() const { return v; }
     mi() { v = 0; }
-    mi(long long _v) : v(_v % MOD) { v += (v < 0) * MOD; }
+    mi(long long _v) : v(_v % mod) { v += (v < 0) * mod; }
+    mi operator+(mi b){return v+b.v - (v+b.v >= mod ? mod : 0);}
+    mi operator-(mi b){return v-b.v + (v-b.v < 0 ? mod : 0);}
+    mi operator*(mi b){return 1ll*v*b.v%mod;}
+    friend mi fexp(mi a, ll b){
+        mi ans = 1;
+        while(b){
+            if(b&1) ans = ans * a;
+            b >>= 1;
+            a = a * a;
+        }
+        return ans;
+    }
+    mi inv(mi a){return fexp(a,mod-2);}
+    mi operator/(mi b){return a * inv(b);}
+    void operator+=(mi b){if((v += b.v) >= mod) v -= mod;}
+    void operator-=(mi b){if((v -= b.v) < 0) v += mod;}
+    void operator *=(mi b){v = 1ll*v*b.v%mod;}
+    void operator /=(mi b){v = v*inv(b);}
+    friend ostream& operator<<(ostream& out, mi at){return out << at.v;}
 };
-mi &operator+=(mi &a, mi b) {
-    if ((a.v += b.v) >= MOD) a.v -= MOD;
-    return a;
-}
-mi &operator-=(mi &a, mi b) {
-    if ((a.v -= b.v) < 0) a.v += MOD;
-    return a;
-}
-mi operator+(mi a, mi b) { return a += b; }
-mi operator-(mi a, mi b) { return a -= b; }
-mi operator*(mi a, mi b) { return mi((long long)a.v * b.v); }
-mi &operator*=(mi &a, mi b) { return a = a * b; }
-mi mypow(mi a, long long p) {
-    assert(p >= 0);
-    return p == 0 ? 1 : mypow(a * a, p / 2) * (p & 1 ? a : 1);
-}
-mi inv(mi a) {
-    assert(a.v != 0);
-    return mypow(a, MOD - 2);
-}
-mi operator/(mi a, mi b) { return a * inv(b); }
