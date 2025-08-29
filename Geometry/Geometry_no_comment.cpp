d3438755adc30ff77bb740a24f917be490f59371
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-08-29 14:13:15 -0300

    Update Geometry_no_comment.cpp

--- Geometry/Geometry_no_comment.cpp
+++ Geometry/Geometry_no_comment.cpp
@@ -6,170 +6,166 @@
 using dd = long double;
 const dd pi = acosl(-1.0);
 const dd eps = 1e-9;
-const dd inf = 1e10;
 #define rad(x) ((x)*pi/180)
 #define sq(x) ((x)*(x))
 namespace geo{
-    using TT = long double;
+    using T = dd;
 
-    bool eq(TT x, TT y){
+    bool eq(const T x,const T y){
         return fabs(x-y) <= eps;
     }
-    inline int signal(TT x){return (((x)>eps) - ((x)<-eps));}
+    int sgn(T x){return (x>eps) - (x<-eps);}
     
     // pode representar um ponto, vetor ou numero complexo
-    struct Point{
-        TT x,y;    
-        Point(TT x_ = 0, TT y_ = 0) : x(x_), y(y_){}
-        bool operator < (const Point & ot) const {
-            if(!eq(x,ot.x)) return x < ot.x; // ordena por x primeiro 
-            if(!eq(y,ot.y)) return y < ot.y; // depois ordena por y
-            return false;
+    struct pt{
+        T x,y;
+        pt(T x_=0, T y_=0) : x(x_),y(y_){}
+        bool operator<(const pt &ot)const{
+            if(!eq(x,ot.x)) return x < ot.x; // ordena por x primeiro
+            return y<ot.y; // ordena por y
         }
-        bool operator == (const Point & ot) const {
-            return eq(x, ot.x) && eq(y, ot.y);
-        }
-        void operator +=(const Point &ot){
-            x += ot.x;
-            y += ot.y;
-        }
-        void operator -=(const Point &ot){
-            x -= ot.x;
-            y -= ot.y;
-        }
-        void operator *=(TT r){
-            x *= r;
-            y *= r;
-        }
-        void operator /=(TT r){
-            x /= r;
-            y /= r;
-        }
-        // operacoes de vetores/complexos
-        TT abs2()const{return sq(x) + sq(y);} 
-        dd abs()const{return sqrtl(abs2());} 
-        Point unit()const{return (*this)/abs();} 
-        dd arg()const{
-            dd ang = atan2(y,x);
-            return (ang < 0 ? ang + 2*pi : ang);
-        }
-        Point perp()const{return Point(-y,x);}
-        // operacoes de complexos (x,y) -> x + y*i
-        Point conj()const{return Point(x,-y);} 
+        bool operator==(const pt &ot)const{return eq(x,ot.x) && eq(y,ot.y);}
+        pt operator-(pt p){return pt(-p.x,-p.y);}
+        pt operator+=(pt b){x+=b.x;y+=b.y;}
+        pt operator-=(pt b){x-=b.x;y-=b.y;}
+        pt operator*=(T r){x*=r;y*=r;}
+        pt operator/=(T r){x/=r;y/=r;}
     };
-    Point operator-(const Point &at){return Point(-at.x,-at.y);}
-    Point operator +(const Point &p1,const Point &p2){
-        return Point(p1.x + p2.x, p1.y + p2.y);
-    }
-    Point operator -(const Point &p1, const Point &p2){
-        return Point(p1.x - p2.x, p1.y - p2.y);
-    }
-    Point operator *(const Point &at, TT r){
-        return Point(at.x * r, at.y * r);
-    }
-    Point operator*(TT r, const Point& at){
-        return Point(at.x * r, at.y * r);
-    }
-    Point operator /(const Point &at, TT r){
-        return Point(at.x / r, at.y / r);
-    }
-    Point dir(TT ang){return Point(cosl(ang), sinl(ang));} 
-    // operacoes de complexo
-    Point operator*(const Point &p1, const Point &p2) {return Point(p1.x*p2.x - p1.y*p2.y, p1.y*p2.x + p1.x*p2.y);}
-    Point operator/(const Point &p1, const Point &p2) {return p1*p2.conj()/p2.abs2();}
-    TT dot(const Point &p1, const Point &p2){
-        return p1.x*p2.x + p1.y*p2.y;
-    }
-    TT dot(const Point & origin, const Point &p1, const Point &p2){
-        return dot(p1-origin,p2-origin);
-    }
-    TT cross(const Point &p1, const Point &p2){
-        return p1.x*p2.y - p1.y*p2.x;
-    }
-    TT cross(const Point &origin, const Point &p1, const Point&p2){
-        return cross(p1-origin,p2-origin);
-    }
-    Point rotate90(const Point &center, const Point &p){
-        Point rotated = p - center;
-        swap(rotated.x,rotated.y);
-        rotated.x = -rotated.x;
-        rotated += center;
-        return rotated;
-    }
-    Point rotate(const Point &center, const Point &p, dd ang){
-        Point rotated = p - center;
-        rotated = Point(rotated.x*cos(ang) - rotated.y*sin(ang), rotated.y*cos(ang) + rotated.x*sin(ang));
-        rotated += center;
-        return rotated;
-    }
-    dd dist(const Point &p1, const Point &p2){
-        return (p1-p2).abs();
-    }
-    TT dist2(const Point &p1, const Point &p2){
-        return (p1-p2).abs2();
-    }
-    dd det_area(const Point &p1, const Point &p2, const Point &p3){
-        return cross(p2-p1,p3-p2)/TT(2);
-    }
-    bool colinear(const Point &p1, const Point &p2, const Point &p3){
-        return eq(det_area(p1,p2,p3),0);
-    }
-    dd angle(const Point &p1, const Point &p2, const Point &p3){
-        Point v1 = p1 - p2, v2 = p3 - p2;
-        dd ang = acosl(dot(v1,v2)/v1.abs()/v2.abs());
-        return (ang < 0 ? ang + 2*pi : ang);
-    }
-    istream& operator >>(istream& in, Point& p) {
-        return in >> p.x >> p.y;
-    }
-    ostream& operator <<(ostream& out, const Point & p){
+    ostream& operator<<(ostream& out, const Point p){
         return out << "(" << p.x << "," << p.y << ")";
     }
 
     // representa uma reta ou segmento, se p == q da merda
-    struct Line{
-        Point p,q;
-        Line(){}
-        Line(const Point &p1, const Point &p2) : p(p1.x,p1.y), q(p2.x,p2.y){}
-        TT get_y(TT x){
-            TT m = (p.y - q.y)/(p.x - q.x);
-            TT b = p.y - m*p.x;
-            return m*x + b;
+    stuct line{
+        pt p,q;
+        line(pt p_={0,0},pt q_={0,0}) : p(p_),q(q_){}
+        T get_y(T x){
+            T m = (p.y - q.y)/(p.x - q.x);
+            T b = p.y - m*p.x;
+            return m*x+b;
         }
-        TT get_x(TT y){
-            TT m = (p.y - q.y)/(p.x - q.x);
-            TT b = p.y - m*p.x;
+        T get_x(T y){
+            T m = (p.y - q.y)/(p.x - q.x);
+            T b = p.y - m*p.x;
             return (y-b)/m;
         }
     };
-    istream& operator >>(istream& in, Line& l) {
-        return in >> l.p >> l.q;
-    }
-    ostream& operator <<(ostream& out, const Line & l){
+    ostream& operator<<(ostream& out, const line l){
         return out << "(" << l.p << ", " << l.q << ")";
     }
-    Point reflect(const Point &p, const Line &l){
-        Point aux1 = l.p, aux2 = l.q-l.p; // ponto p e vetor p->q
-        return aux1 + ((p-aux1)/aux2).conj()*aux2;
+
+    pt operator+(pt a,pt b){return a+=b};
+    pt operator-(pt a,pt b){return a-=b;}
+    pt operator*(pt a, T r){return a*=r;}
+    pt operator/(pt a, T r){return a/=r;}
+    // operacoes de vetores/complexos
+    T abs2(pt p){return sq(p.x) + sq(p.y);}
+    dd abs(pt p){return sqrtl(abs2(p));}
+    pt unit(pt p){return p/abs(p);}
+    dd arg(pt p){
+        dd ang = atan2(y,x);
+        return (ang < 0 ? ang + 2*pi : ang);
     }
-    Point proj(const Point &p, const Line &l){
-        return (p+reflect(p,l))/TT(2);
+    pt conj(pt p){return pt(p.x,-p.y);}
+    pt perp(pt p){return pt(-p.y,p.x);}
+    pt dir(T ang){return pt(cosl(ang),sinl(ang));}
+    
+    // operacoes de complexos
+    pt operator*(pt a, pt b){return pt(a.x*b.x-a.y*b.y,a.y*b.x+a.x*b.y);}
+    pt operator/(pt a, pt b){return a*conj(b)/abs2(b);}
+    
+    T dot(pt p1, pt p2){
+        return p1.x*p2.x+p1.y*p2.y;
     }
-    bool onSeg(const Point &p, const Line &l){
-        return signal(cross(l.p,l.q,p)) == 0 && signal(dot(p,l.p,l.q)) <= 0;
+    T dot(pt p1, pt p2, pt p3){
+        return dot(p2-p1,p3-p1);
     }
-    bool onLine(const Point &p, const Line &l){
-        return signal(cross(l.p,l.q,p)) == 0;
+    T cross(pt p1, pt p2){
+        return p1.x*p2.y-p1.y*p2.x;
     }
-    dd line_dist(const Line &line, const Point &p){
-        dd ans = abs(cross(p,line.p,line.q));
-        ans /= (line.p-line.q).abs();
-        return ans;
+    T cross(pt p1, pt p2, pt p3){
+        return cross(p2-p1,p3-p1);
     }
-    dd seg_dist(const Line &seg, const Point &p){
-        if(dot(seg.p, p, seg.q) <= 0) return (p-seg.p).abs();
-        if(dot(seg.q, p, seg.p) <= 0) return (p-seg.q).abs();
-        return line_dist(seg,p);
+    
+    pt reflect(const pt p, const line l){
+        pt a = l.p, d = l.q-l.p;
+        return a+conj((p-a)/d)*d;
     }
-}
+    pt proj(const pt p, const line l){
+        return (p+reflect(p,l))/(T)2;
+    }
+    pt rotate90(pt a, pt c = pt(0,0)){
+        a -= c;
+        swap(a.x,a.y);
+        a.x = -a.x;
+        a += c;
+        return a;
+    }
+    pt rotate(pt a, dd d, pt c = pt(0,0)){
+        a -= c;
+        pt b;
+        b.x = a.x*cosl(d) - a.y*sinl(d);
+        b.y = a.y*cosl(d) + a.x*sinl(d);
+        b += c;
+        return b;
+    }
+
+    bool onseg(pt p, line l){
+        return sng(cross(l.p,l.q,p)) == 0 && sgn(dot(p,l.p,l.q)) <= 0;
+    }
+    bool online(pt p, line l){return sgn(cross(l.p,l.q,p)) == 0;}
+    dd dist(pt p1, pt p2){return abs(p2-p1);}
+    T dist2(pt p1, pt p2){return abs2(p2-p1);}
+    T det(pt p1, pt p2, pt p3){return cross(p2-p1,p3-p2);}
+    bool colinear(pt p1,pt p2,pt p3){return eq(det(p1,p2,p3),0);}
+    dd ang(pt p1, pt p2, pt p3){
+        pt v1 = p1-p2, v2 = p3-p2;
+        dd angle = acosl(dot(v1,v2)/abs(v1)/abs(v2));
+        return (ang < 0 ? ang + 2*pi : ang);
+    }
+    dd linedist(const line l, const pt p){
+        return abs(cross(p,l.p,l.q))/abs(l.p-l.q);
+    }
+    dd segdist(const line seg, const pt p){
+        if(dot(seg.p, p, seg.q) <= 0) return abs(p-seg.p);
+        if(dot(seg.q, p, seg.p) <= 0) return abs(p-seg.q);
+        return line_dist(seg,p);    
+    }
+
+    //  Usage: vpt v; sort(all(v),angleCmp);
+    // used to sort points int ccw around the origin
+    int half(pt p){return !eq(p.y,0) ? sgn(p.y) : -sgn(p.x);}
+    bool angleCmp(pt a, pt b){
+        int A = half(a), B = half(b);
+        return A == B ? cross(a,b) > 0 : A < B;
+    }
+    // equivalent to: sort(all(v),[](pt a, pt b) { 
+    //    return atan2(a.sd,a.ft) < atan2(b.sd,b.ft); });
+
+    using vpt = vector<pt>;
+    // {unique intersection point} if it exists
+    // {b.f,b.s} if input lines are the same
+    // empty if lines do not intersect
+    vpt lineIsect(const line a, const line b){
+        T a0 = cross(a.p,a.q,b.p), a1 = cross(a.p,a.q,b.q);
+        if(eq(a0,a1)) return eq(a0,0) ? vpt{b.p,b.q} : vpt{};
+        return {(b.q*a0-b.p*a1)/(a0-a1)};
+    }
+    // point in interior of both segments a and b, if it exists
+    vpt strictIsect(const line a, const line b){
+        T a0 = cross(a.p,a.q,b.p), a1 = cross(a.p,a.q,b.q);
+        T b0 = cross(b.p,b.q,a.p), b1 = cross(b.p,b.q,a.q);
+        if(sgn(a0)*sgn(a1) < 0 && sgn(b0)*sgn(b1) < 0) return {(b.q*a0-b.p*a1)/(a0-a1)};
+        return {};
+    }
+    // intersection of segments, a and b may be degenerate
+    vpt segIsect(const line a, const line b){
+        vpt v = strictIsect(a,b); if(!v.empty()) return v;
+        set<pt> s;
+        #define i(x,y) if(onseg(x,y)) s.insert(x)
+        i(a.p,b); i(a.q,b); i(b.p,a); i(b.q,a);
+        return vpt{s.begin(),s.end()};
+    }
+
+};
 using namespace geo;
