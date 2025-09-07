5e8d355d002457d51205b2c5f07cf57fc1bc1893
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-09-07 00:00:07 -0300

    Update Linear System.cpp

--- Math/Linear System.cpp
+++ Math/Linear System.cpp
@@ -1,71 +1,89 @@
-// if using modular gauss, change THAT PART and use mint
 namespace Gauss{
-    using vi = vector<int>;
     const double eps = 1e-12;
 
     template <class TT>
-    void show(vector<vector<TT>>& A, vector<TT>& b){
-        int n = A.size(), m = b.size();
+    void show(vector<vector<TT>>& a, vector<TT>& b){
+        if(a.size() == 0) return;
+        int n = a.size(), m = a[0].size();
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++) cout << A[i][j] << ' ';
+            for(int j = 0; j < m; j++) cout << a[i][j] << ' ';
             cout << "= " << b[i] << '\n';
         }
     }
 
-    template <class TT>
-    int solveLinear(vector<vector<TT>>& A, vector<TT>& b, vector<TT>& x) {
-        int n = A.size(), m = x.size(), rank = 0, br, bc;
-        if (n) assert(A[0].size() == m);
-        vi col(m); iota(col.begin(),col.end(), 0); // fills the range with increasing values starting with value 0
+    // search for assert(1 == 0) to make necessary uncomments about modint or double
+    template <class TT> vector<vector<TT>> solveLinear(vector<vector<TT>> a, vector<TT> b) {
+        if(a.empty() || a[0].empty()) return {};
+        int n = a.size(), m = a[0].size();
+        assert(int(b.size()) == n);
+        int r = 0;
+        vector<bool> used(m,false);
+        vector<int> idcol;
+        for(int j = 0; j < m; j++){
+            int lin = -1;
+            for(int i = r ; i < n; i++){
+                assert(1 == 0);
+                // if(int(a[i][j])){
+                //     lin = i;
+                //     break;
+                // }
 
-        for(int i = 0; i < n; i++){
-            TT v, bv = 0;
-
-            for(int r = i; r < n; r++) for(int c = i; c < m; c++)
-                if((v = A[r][c]) > bv)
-                    br = r, bc = c, bv = v;
-            if(bv <= eps){
-                for(int j = i; j < n; j++) if(fabs(b[j]) > eps) return -1;
-                break;
+                // if(fabs(a[i][j]) > eps){
+                //     lin = i;
+                //     break;
+                // }
             }
-
-            // THAT PART
-            // for(int r = i; r < n; r++) for(int c = i; c < m; c++)
-            //     if ((v = fabs(A[r][c])) > bv)
-            //         br = r, bc = c, bv = v;
-            // if(bv) {
-            //     for(int j = i; j < n; j++) if(b[j]) return -1;
-            //     break;
-            // }
-            
-            // put in the diagonal
-            swap(A[i], A[br]);
-            swap(b[i], b[br]); 
-            swap(col[i], col[bc]);
-            for(int j = 0; j < n; j++) swap(A[j][i], A[j][bc]);
-            
-            bv = 1/A[i][i]; 
-            // faster, dont update useless values
-            for(int j = i+1; j < n; j++){
-                // A[i][i]*(1/A[i][i] * A[j][i]) = A[i][i]*fac (zero the column)
-                TT fac = A[j][i] * bv; 
-                b[j] -= fac * b[i];
-                for(int k = i+1; k < m; k++) A[j][k] -= fac*A[i][k];
+            if(lin == -1) continue; // zero in column, reason for idcol
+            if(r != lin) swap(a[r],a[lin]); // make diagonal
+            swap(b[r],b[lin]);
+            TT ival = 1/a[r][j];
+            for(int i = r+1; i < n; i++){
+                assert(1 == 0);
+                // if(!int(a[i][j])) continue;
+                // if(fabs(a[i][j]) > eps) continue;
+                auto freq = a[i][j] * ival;
+                for(int k = j; k < m; k++) a[i][k] -= freq * a[r][k];
+                b[i] -= freq * b[r];
             }
-            // slower, turn lines to zero
-
-            // zero the column
-            rank++;
+            r++;
+            used[j] = true;
+            idcol.push_back(j);
+            if(r == n) break;
         }
-
-        // variable values
-        x.assign(m, 0);
-        for (int i = rank; i--;) {
-            b[i] /= A[i][i];
-            x[col[i]] = b[i];
-            for(int j = 0; j < i; j++) b[j] -= A[j][i] * b[i]; // o valor da variavel x_i eu descobri agora eu desconto o valor dela nas equacoes acima
+        // r is the rank of the matrix
+        for(int i = r; i < n; i++){
+            assert(1 == 0);
+            // if(int(b[i])) return {};
+            // if(fabs(b[i]) > eps) return {};
         }
-        return rank; // (multiple solutions if rank < m)
+        // random solution,vectors that form solution basis
+        // Solution = X0 + linear_combination_of_V
+        // sols = {X0,V1,V2,V3,...,V_r}
+        vector<vector<TT>> sols;
+        // solution X0
+        {
+            vector<TT> v(m,0);
+            for(int i=r-1; i >= 0; i--){
+                int id = idcol[i];
+                v[id] = b[i];
+                for(int j=id+1; j<m; j++) v[id] -= a[i][j] * v[j];
+                v[id] /= a[i][id];
+            }
+            sols.push_back(v);
+        }
+        // free variables
+        for(int s=0; s<m; s++){
+            if(used[s]) continue;
+            vector<TT> v(m,0);
+            v[s] = 1;
+            for(int i=r-1; i>=0; i--){
+                int id = idcol[i];
+                for(int j=id+1; j<m; j++) v[id] -= a[i][j] * v[j];
+                v[id] /= a[i][id];
+            }
+            sols.push_back(v);
+        }
+        return sols;
     }
 }
 using namespace Gauss;
