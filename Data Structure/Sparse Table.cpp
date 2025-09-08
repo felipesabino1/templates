b99606ae1ddbf404ec6772e077b6d08839d2cc48
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-09-08 10:41:37 -0300

    Update Sparse Table.cpp

--- Data Structure/Sparse Table.cpp
+++ Data Structure/Sparse Table.cpp
@@ -12,53 +12,16 @@ struct SparseTable{
     int n; // tamanho 
     vector<vector<TT>> tab; // sparse table
     vector<int> pot2; // log de cada valor
-    int TETO; // Teto do log2(n)
     const TT off = ;
 
-    SparseTable(int n_) : n(n_){
-        pot2.resize(n+10);
+    SparseTable(int n_) : n(n_), pot2(n_+10){
         pot2[1] = 0;
         for(int i=2; i<=n; i++) pot2[i] = pot2[i>>1] + 1;
         tab.resize(n+10,vector<TT>(pot2[n]+1));
-        TETO = pot2[n]+1;    
     }
-    SparseTable(int n_, vector<TT> & a) : n(n_){
-        pot2.resize(n+10);
-        pot2[1] = 0;
-        for(int i=2; i<=n; i++) pot2[i] = pot2[i>>1] + 1;
-        tab.resize(n+10,vector<TT>(pot2[n]+1));
-        TETO = pot2[n]+1;
-        build(a); // build da Sparse Table
-    }
-    SparseTable(int n_, TT * a) : n(n_){
-        pot2.resize(n+10);
-        pot2[1] = 0;
-        for(int i=2; i<=n; i++) pot2[i] = pot2[i>>1] + 1;
-        tab.resize(n+10,vector<TT>(pot2[n]+1));
-        TETO = pot2[n]+1;
-        build(a); // build da Sparse Table
-    }
-
-    ~SparseTable(){tab.clear(); pot2.clear();}
-
-    void init(int n_new,vector<TT> & a){
-        n = n_new;
-        TETO = pot[n] + 1;
-        build(a);
-    }
-    void init(int n_new,TT * a){
-        n = n_new;
-        TETO = pot[n] + 1;
-        build(a);
-    }
-
-    void build(vector<TT> & a){
-        for(int i=1; i<=n; i++) tab[i][0] = a[i];
-        for(int j=1; j<TETO; j++) for(int i=0; i+(1<<j)-1 <= n; i++) tab[i][j] = f(tab[i][j-1],tab[i+(1<<(j-1))][j-1]);
-    }
-    void build(TT * a){
-        for(int i=1; i<=n; i++) tab[i][0] = a[i];
-        for(int j=1; j<TETO; j++) for(int i=0; i+(1<<j)-1 <= n; i++) tab[i][j] = f(tab[i][j-1],tab[i+(1<<(j-1))][j-1]);
+    void build(){
+        for(int i=1; i<=n; i++) tab[i][0] = a[i]; // nome do vetor original
+        for(int j=1; j<=pot2[n]; j++) for(int i=1; i+(1<<j)-1 <= n; i++) tab[i][j] = f(tab[i][j-1],tab[i+(1<<(j-1))][j-1]);
     }
 
     // operacao da sparse table
@@ -69,7 +32,7 @@ struct SparseTable{
     TT query(int l,int r){
         if(l > r) return off;
         TT ans = off;
-        for(int j=TETO-1; j>=0; j--){
+        for(int j=pot2[n]; j>=0; j--){
             if(l+(1<<j)-1 <= r){
                 ans = f(ans, tab[l][j]);
                 l +=(1<<j);
