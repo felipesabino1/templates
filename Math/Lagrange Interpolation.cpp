9162e4d6a55ddbd46044289489cc1913e891985f
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-09-08 09:35:41 -0300

    Update Lagrange Interpolation.cpp

--- Math/Lagrange Interpolation.cpp
+++ Math/Lagrange Interpolation.cpp
@@ -1,99 +1,71 @@
 /*
     Alteracoes:
 
-    Alterar a funcao g de dentro da funcao de lagrange
-    Na funcao que o faz o lagrange mesmo, alterar os parametros da funcao g de acordo com o que foi faz
+    Precisa de modint
+    Alterar a funcao f que calcula os valores y do polinomio
+    O(N) tal que N eh o grau do polinomio
 */
 struct Lagrange{
-    vector<ll> pref,suf;
-    vector<ll> invfat;
-    vector<ll> y;
-    // valor x que quer calcular e o grau do polinomio n
-    ll x,n;
-    ll mod;
+    vector<mi> pref,suf,invfat,y;
+    // valor x que quer calcular e o grau n do polinomio
+    ll x;
+    int n;
     // geralmente o negocio da funcao precisa de mais informacao
     ll pte;
 
-    void set__(){
-        pref[0]=1;
-        suf[n+2]=1;
-        for(ll i=1; i<=n+1; i++) pref[i]=pref[i-1]*((x-i)%mod)%mod;
-        for(ll i=n+1; i>0; i--) suf[i]=suf[i+1]*((x-i)%mod)%mod;
-        ll fat=1;
-        invfat[0]=1;
-        for(ll i=1; i<=n+1; i++){
-            fat=fat*i%mod;
-            invfat[i]=fexp(fat,mod-2);
-        }
-        y[0]=0;
-        for(int i=1; i<=n+1; i++) y[i]=(y[i-1]+g())%mod;
-    }
-
-    void set2__(){
-        pref[0]=1;
-        suf[n+2]=1;
-        for(ll i=1; i<=n+1; i++) pref[i]=pref[i-1]*((x-i)%mod)%mod;
-        for(ll i=n+1; i>0; i--) suf[i]=suf[i+1]*((x-i)%mod)%mod;
-    }
-
-    Lagrange(ll x, ll n,ll mod, ll pte) : x(x),n(n),mod(mod),pte(pte){
-        pref.resize(n+3);
-        suf.resize(n+3);
-        invfat.resize(n+3);
-        y.resize(n+3);
-        
-        set__();
-    }
-
-    // exponenciacao binaria
-    ll fexp(ll a, ll b){
-        a%=mod;
-        ll ans=1;
+    mi fexp(mi a, ll b){
+        mi ans = 1;
         while(b){
-            if(b&1) ans=ans*a%mod;
-            b>>=1;
-            a=a*a%mod;
+            if(b&1) ans *= a;
+            b >>= 1;
+            a *= a;
         }
         return ans;
     }
-
-    // se eu so quiser mudar o x que eu calculo do polinomio, so preciso alterar o prefixo e o sufixo
-    void mudax(ll x){
-        this->x = x;
-
-        set2__();
-    }
-
-    // funcao de dentro do polinomio de lagrange
-    ll g(){
+    // funcao que calcula os valores do polinomio real
+    mi f(){
         
     }
+    Lagrange(ll xx, int nn, ll ptee) : pref(nn+3),suf(nn+3),invfat(nn+3),y(nn+3),x(xx),n(nn),pte(ptee){
+        pref[0]=1; suf[n+2]=1;
+        for(int i=1; i<=n+1; i++) pref[i]=pref[i-1]*(x-i);
+        for(int i=n+1; i>0; i--) suf[i]=suf[i+1]*(x-i);
 
-    // o que calcula o valor do polinomio no ponto n, com grau k (lg - lagrange)
-    ll lg(){
+        mi fat = 1;
+        for(int i=1; i<=n+1; i++) fat *= i;
+        invfat[n+1] = 1/fat;
+        for(int i=n; i>=0; i--) invfat[i] = invfat[i+1]*(i+1);
+        
+        y[0]=0;
+        // na maioria das vezes o y vai ser um somatorio dos anteriores, mas pode n acontecer, y[i] eh o valor do polinomio real em i 
+        for(int i=1; i<=n+1; i++) y[i]= y[i-1]+f();
+    }
+
+    // se eu so quiser mudar o x que eu calculo do polinomio, so preciso alterar o prefixo e o sufixo
+    void mudaX(ll xx){
+        this->x = xx;
+        pref[0]=1;
+        suf[n+2]=1;
+        for(int i=1; i<=n+1; i++) pref[i]=pref[i-1]*(x-i);
+        for(int i=n+1; i>0; i--) suf[i]=suf[i+1]*(x-i);        
+    }
+
+    // o que calcula o valor do polinomio no ponto X, com grau N
+    mi lg(){
+        mi ans = 0;
         if(x <= n+1){
-            ll ans=0;
-            // calcula o somatorio ate o n normal
-            for(int i=1; i<=x; i++) ans=(ans+g())%mod;
-            return ans;
+            // so calcula o valor do polinomio
+            ans = y[x];
         }else{
-            ll ans=0;
             for(int i=1; i<=n+1; i++){
-                // g(i)*produtorio(x-j)/produtorio(i-j)
-                ll aux=y[i]*pref[i-1]%mod;
-                aux=aux*suf[i+1]%mod;
-                aux=aux*invfat[n+1-i]%mod;
-                aux=aux*invfat[i-1]%mod;
-
+                // f(i)*produtorio(x-j)/produtorio(i-j)
+                mi aux = y[i] * pref[i-1] * suf[i+1] * invfat[n+1-i] * invfat[i-1];
                 // o -1 do produtorio(i-j)
-                if((n+1-i)&1) aux*=-1;
-                while(aux < 0) aux+=mod;
-
-                // adiciona esse cara no polinomio de lagrange
-                ans=(ans+aux)%mod;
+                if((n+1-i)&1) aux *= -1;
+                // adiciona essa funcao auxiliar
+                ans += aux;
             }
-
-            return ans;
         }
+        return ans;
     }
 };
