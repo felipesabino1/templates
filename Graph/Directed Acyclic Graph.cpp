c81adc12ea6773afb81e32a4a38c2739eec98707
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-09-11 08:09:51 -0300

    Update Directed Acyclic Graph.cpp

--- Graph/Directed Acyclic Graph.cpp
+++ Graph/Directed Acyclic Graph.cpp
@@ -1,34 +1,9 @@
 /*
-    Indexado de 1
+    Indexado de 1. O grafo condensado ta em ngraph, tem que ter um graph e inv globais
 */
-struct DAG{
-    int n,timer=1;
-    vector<int> vis,ord; // no visitado vai ser qual a componente de cada vertice, o timer tem a qtd de componentes total
-    vector<vector<int>> graph,inv; // grafo original
-    vector<vector<int>> ngraph; // novo grafo
-
-    DAG(int n) : n(n){
-        vis.resize(n+10,0);
-        graph.resize(n+10);
-        inv.resize(n+10);
-    }
-    ~DAG(){
-        vis.clear();
-        ord.clear();
-        graph.clear();
-        inv.clear();
-        ngraph.clear();
-    }
-
-    void read_edge(int m){
-        int x,y;
-        for(int i=0; i<m; i++){
-            cin >> x >> y;
-            graph[x].push_back(y);
-            inv[y].push_back(x);
-        }
-    }
-
+namespace DAG{
+    int timer;
+    vector<int> vis,ord,ngraph;
     void set_ord(int u){
         vis[u] = 1;
         for(auto v: graph[u]){
@@ -37,7 +12,6 @@ struct DAG{
         }
         ord.push_back(u);
     }
-
     void set_comp(int u){
         vis[u] = timer;
         for(auto v: inv[u]){
@@ -45,17 +19,18 @@ struct DAG{
             set_comp(v);
         }
     }
-
-    void solve(){
+    void solve(int n){
+        vis.resize(n); ord.clear();
         // ordem de saida dos caras
         for(int i=1; i<=n; i++) vis[i] = 0;
         for(int i=0; i<=n; i++) if(!vis[i]) set_ord(i);
         reverse(ord.begin(),ord.end());
         for(int i=1; i<=n; i++) vis[i] = 0;
         // andar no grafo inverso pra arrumar as componentes
+        timer = 1;
         for(auto u : ord) if(!vis[u]) set_comp(u),timer++;
         timer--;
-        ngraph.resize(timer+10);
+        ngraph.resize(timer+1);
         // criar o novo grafo
         for(int i=1; i<=n; i++){
             int u = vis[i];
@@ -66,4 +41,5 @@ struct DAG{
             }
         }
     }
-};
+}
+using namespace DAG;
