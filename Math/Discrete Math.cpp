9ed6eb74d6b65ad1661f899292c830e2388745da
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-10-13 10:05:40 -0300

    Update Discrete Math.cpp

--- Math/Discrete Math.cpp
+++ Math/Discrete Math.cpp
@@ -1,55 +1,48 @@
 namespace MD{
     const ll off = -1; // valor que indica que deu erro em conta
-    ll mygcd(ll a, ll b){
-        if(a < 0) a*=-1; if(b < 0) b*=-1; if(a > b) swap(a,b);
-        ll aux;
-        while(a != 0){
-            aux = b%a;
-            b = a;
-            a = aux;
-        }
-        return b;
+
+    // Euclides estendido: acha x,y tal que ax + by = gcd(a,b)
+    // Assume que a,b >= 0, pra converter dps eh facil
+    // O(log(min(a,b)))
+    tuple<ll,ll,ll> ext_gcd(ll a, ll b){
+        if(!a) return {b, 0, sig};
+        auto [g,x,y] = ext_gcd(b%a,a);
+        return {g,y - b/a*x,x};
     }
-    // acha os coeficientes de a*x + b*y = gcd(a,b)
-    pair<ll,ll> extended_euclides(ll a, ll b){
-        bool trocou = 0, nega = 0, negb = 0;
-        if(a < 0) nega=1,a*=-1; if(b < 0) negb=1,b*=-1; if(a > b) swap(a,b),trocou = 1;
-        vector<pair<ll,ll>> st;
-        ll aux,g,s,t;
-        while(a != 0){
-            st.push_back(make_pair(a,b));
-            aux = b%a; b = a; a = aux;
+    // Combina equacoes modulares lineares: x = a (mod m)
+    // O m final eh o lcm dos m's, e a resposta eh unica mod lcm
+    // Os m nao precisam ser coprimos e se nao tiver solucao, o 'a' vai ser -1
+    struct crt{
+        ll a, m;
+        crt(ll aa = 0, ll mm = 1) : a(aa), m(mm) {}
+        crt operator*(crt c){
+            auto [g,x,y] = ext_gcd(m,c.m);
+            if((a-c.a)%g) a = -1;
+            if(a == -1 || c.a == -1) return crt(-1,0);
+            // if(lcm > inf) ai nem precisa de CRT que o a ja vai ser a resposta
+            ll lcm = m/g*c.m;
+            ll ans = a + (x*(c.a-a)/g % (c.m/g)) * m;
+            return crt((ans%lcm + lcm)%lcm,lcm);
         }
-        g = b, s = 0, t = 1;
-        while(!st.empty()){
-            a = st.back().first, b = st.back().second, t = s;
-            s = (g-t*b)/a;
-            st.pop_back();  
-        }
-        if(nega) t*=-1,s*=-1;
-        if(trocou) return make_pair(t,s);
-        else return make_pair(s,t);
-    }
+    };
     // calcula o inverso de a modulo m
     ll inv(ll a, ll m){
-        a %= m; if(a < 0) a = m+a;
-        if(gcd(a,m) != 1) return off; // nao existe inverso modular
-        pair<ll,ll> coef = extended_euclides(a,m);
-        return coef.first%m;
+        assert(m > 0);
+        a %= m; if(a < 0) a += m;
+        auto [g,x,y] = ext_gcd(a,m);
+        if(g != 1) return off;
+        return x%m + (x < 0 ? m : 0);
     }
     // resolve equacoes do tipo a*x = b (mod m)
     ll solveLinear(ll a, ll b, ll m){
-        a %= m; if(a < 0) a = m+a;
-        b %= m; if(b < 0) b = m+b;
-        ll g;
-        while((g = gcd(a,m)) != 1){
-            if(b % g != 0) return off; // nao eh uma relacao possivel
-            a/=g, b/=g, m/=g;
-        }
-        a %= m; b %= m;
-        pair<ll,ll> coef = extended_euclides(a,m);
-        b = b*(coef.first%m)%m;
-        return b;
+        assert(m > 0);
+        a %= m; if(a < 0) a += m;
+        b %= m; if(b < 0) b += m;
+        ll g = __gcd(a,m);
+        if(b%g) return off;
+        a /= g, b /= g, m /= g;
+        auto [g,x,y] = ext_gcd(a,m);
+        return b*(x%m)%m;
     }
 }
 using namespace MD;
