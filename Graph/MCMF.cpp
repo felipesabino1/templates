1b99d3e7716d880c5c8ed53c2d4bdedccc918450
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-10-21 10:11:08 -0300

    Update MCMF.cpp

--- Graph/MCMF.cpp
+++ Graph/MCMF.cpp
@@ -1,16 +1,18 @@
-// tipo do fluxo, sempre tem que ser inteiro,
-// tipo do custo.
+// Indexado de 0
+// Se for um dag, da pra substituir o SPFA por uma DP pra nao
+// pagar O(nm) no comeco
+// Se nao tiver aresta com custo negativo, nao precisa do SPFA
+// O(nm + f * m log n)
 template <class TT = int, class TT1 = ll>
-class MCMF {
-public:
+struct MCMF {
     struct Edge{
         int from,to;
-        TT cap;
+        TT cap,flow = 0;
         TT1 cost;
-        Edge(int a, int b, TT c, TT1 d) : from(a), to(b), cap(c), cost(d) {}
+        Edge(int aa = 0, int bb = 0, TT cc = 0, TT1 ee = 0) : from(aa), to(bb), cap(cc), cost(ee) {}
     };
 
-    MCMF(int size) : n(size), edges(n), pot(n,0), dist(n), visit(n,false){}
+    MCMF(int size) : n(size), edges(n), dist(n), pot(n,0), visit(n,false), inf(numeric_limits<TT1>::max()){}
 
     pair<TT, TT1> mcmf(int src, int sink) {
         pair<TT, TT1> ans(0, 0);
@@ -26,65 +28,62 @@ public:
         return ans;
     }
 
-    void add_edge(int from, int to, TT cap, TT1 cost) {
-        edges[from].push_back(list.size());
-        list.emplace_back(from,to, cap, cost));
+    void add_edge(int fr, int to, TT cap, TT1 cost) {
+        edges[fr].push_back(list.size());
+        list.emplace_back(fr, to, cap, cost);
         edges[to].push_back(list.size());
-        list.emplace_back(to,from,0,-cost);
+        list.emplace_back(to,fr,0,-cost);
     }
-private:
+
     int n;
     vector<vector<int>> edges;
     vector<Edge> list;
     vector<int> from;
     vector<TT1> dist, pot;
     vector<bool> visit;
+    TT1 inf;
 
-    /*bool dij(int src, int sink) {
-        TT1 INF = numeric_limits<TT1>::max();
-        dist.assign(n, INF);
-        from.assign(n, -1);
-        visit.assign(n, false);
-        dist[src] = 0;
-        for(int i = 0; i < n; i++) {
-            int best = -1;
-            for(int j = 0; j < n; j++) {
-                if(visit[j]) continue;
-                if(best == -1 || dist[best] > dist[j]) best = j;
-            }
-            if(dist[best] >= INF) break;
-            visit[best] = true;
-            for(auto e : edges[best]) {
-                auto ed = list[e];
-                if(ed.cap == 0) continue;
-                TT1 toDist = dist[best] + ed.cost + pot[best] - pot[ed.to];
-                assert(toDist >= dist[best]);
-                if(toDist < dist[ed.to]) {
-                    dist[ed.to] = toDist;
-                    from[ed.to] = e;
-                }
-            }
-        }
-        return dist[sink] < INF;
-    }*/
+    // bool dij(int src, int sink) {
+    //     dist.assign(n, inf);
+    //     from.assign(n, -1);
+    //     visit.assign(n, false);
+    //     dist[src] = 0;
+    //     for(int i = 0; i < n; i++) {
+    //         int best = -1;
+    //         for(int j = 0; j < n; j++) if(!visit[j] && (best == -1 || dist[best] > dist[j])) best = j;
+    //         if(dist[best] >= inf) break;
+    //         visit[best] = true;
+    //         for(auto e : edges[best]) {
+    //             auto ed = list[e];
+    //             if(ed.flow == ed.cap) continue;
+    //             TT1 toDist = dist[best] + ed.cost + pot[best] - pot[ed.to];
+    //             assert(toDist >= dist[best]);
+    //             if(toDist < dist[ed.to]) {
+    //                 dist[ed.to] = toDist;
+    //                 from[ed.to] = e;
+    //             }
+    //         }
+    //     }
+    //     return dist[sink] < inf;
+    // }
 
     pair<TT, TT1> augment(int src, int sink) {
-        pair<TT, TT1> flow = {list[from[sink]].cap, 0};
-        for(int v = sink; v != src; v = list[from[v]^1].to) {
-            flow.first = min(flow.first, list[from[v]].cap);
-            flow.second += list[from[v]].cost;
+        pair<TT, TT1> flow = {list[from[sink]].cap - list[from[sink]].flow, 0};
+        for(int v = sink; v != src; v = list[from[v]^1].to){
+            auto e = list[from[v]];
+            flow.first = min(flow.first, e.cap - e.flow);
+            flow.second += e.cost;
         }
         for(int v = sink; v != src; v = list[from[v]^1].to) {
-            list[from[v]].cap -= flow.first;
-            list[from[v]^1].cap += flow.first;
+            list[from[v]].flow += flow.first;
+            list[from[v]^1].flow -= flow.first;
         }
         return flow;
     }
 
     queue<int> q;
     bool SPFA(int src, int sink) {
-        TT1 INF = numeric_limits<TT1>::max();
-        dist.assign(n, INF);
+        dist.assign(n, inf);
         from.assign(n, -1);
         q.push(src);
         dist[src] = 0;
@@ -94,7 +93,7 @@ private:
             visit[on] = false;
             for(auto e : edges[on]) {
                 auto ed = list[e];
-                if(ed.cap == 0) continue;
+                if(ed.flow == ed.cap) continue;
                 TT1 toDist = dist[on] + ed.cost + pot[on] - pot[ed.to];
                 if(toDist < dist[ed.to]) {
                     dist[ed.to] = toDist;
@@ -106,13 +105,12 @@ private:
                 }
             }
         }
-        return dist[sink] < INF;
+        return dist[sink] < inf;
     }
 
     void fixPot() {
-        TT1 INF = numeric_limits<TT1>::max();
         for(int i = 0; i < n; i++) {
-            if(dist[i] < INF) pot[i] += dist[i];
+            if(dist[i] < inf) pot[i] += dist[i];
         }
     }
 };
