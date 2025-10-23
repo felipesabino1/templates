b8f8189a659f45d1b7cc2d35466d56e5a29480e4
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-10-23 10:15:50 -0300

    Update Segment Tree.cpp

--- Data Structure/Segment Tree.cpp
+++ Data Structure/Segment Tree.cpp
@@ -5,18 +5,17 @@
 // Update: 4*log(N)
 // Definir o node e sono
 struct Seg{
-    // inicializar a seg
     Seg(int n_) : n(n_), seg(n_<<2), lazy(n_<<2){
         build(1,1,n);
     }
-    // o que vai ter dentro do no de cada seg
+    // No da seg
     struct node{
 
         bool operator ==(const node &ot)const{
             return true;
         }
     };
-    // o que vai ter dentro do no de cada lazy, define o update
+    // Update e lazy
     struct sono{
 
         bool operator ==(const sono &ot)const{
@@ -25,14 +24,11 @@ struct Seg{
     };
 
     int n;
-    // no nulo
     const node off = {};
-    // lazy nula
     const sono off_lazy = {}; 
     vector<node> seg;
     vector<sono> lazy;
     node ret,aux;
-    // operacao de unir dois nos
     void merge(node &x, node &y, node & at){
         if(x == off) {
             at = y;
@@ -50,18 +46,14 @@ struct Seg{
 
     }
     
-    // coisar a lazy pra baixo
     void push(int u,int tl,int tr){
         if(tl == tr || lazy[u] == off_lazy) return;
         int tmid = tl + tr; tmid >>= 1;
         apply(lef(u),tl,tmid,lazy[u]), apply(rig(u),tmid+1,tr,lazy[u]);
         lazy[u] = off_lazy;
     }
-    
-    // inicializar a seg
     void build(int u,int tl,int tr){
         if(tl == tr){
-            // inicializar os caras bases
             seg[u] = {}; lazy[u] = off_lazy;
             return;
         }
@@ -71,7 +63,6 @@ struct Seg{
         lazy[u] = off_lazy;
     }
     
-    // consulta em range
     void query(int u,int tl,int tr,int l, int r){
         if(l > r) return;
         if(tl == l && tr == r) {
@@ -88,11 +79,9 @@ struct Seg{
         return ret;
     }
     
-    // update em range
     void update(int u, int tl, int tr, int l, int r, sono& x){
         if(l > r) return;
         if(tl == l && tr == r){
-            // atualizar a seg e o lazy
             apply(u,tl,tr,x);
             return;
         }
@@ -101,7 +90,7 @@ struct Seg{
         update(lef(u), tl, tmid, l, min(tmid,r), x), update(rig(u), tmid+1, tr, max(tmid+1,l), r, x);
         merge(seg[lef(u)], seg[rig(u)], seg[u]);
     }
-    // passa os parametros e passa um sono
+    // passa os parametros que dai vai converter pra sono
     void update(int l, int r, ll x){
         sono vals = {};
         update(1, 1, n, l, r, vals);
