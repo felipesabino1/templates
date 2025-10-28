e9b5d6b9f1bca7296a2167a5e9622e5f19750921
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-10-28 12:30:44 -0300

    Update DSU-SMOL.cpp

--- Data Structure/DSU-SMOL.cpp
+++ Data Structure/DSU-SMOL.cpp
@@ -1,35 +1,23 @@
-/*
-    Indexado em 1
-
-    Alteracoes:
-
-    Ver se precisa adicionar coisa no gp do smol
-*/
+// Indexado em 1
+// Alterar o que cada grupo tem
 struct DSU{
     // o que cada grupo carrega de informacao
     struct gp{
-        // se precisa adicionar coisa a mais
         int tam = 0;
-
-        // inicializar o smol
         void init(int x){
-            // se precisa inicializar coisa a mais, passa por parametro
             tam=1;
         }
         void clear(){
             tam = 0;
         }
-        ~gp(){}
     };
     
+    int n;
     vector<int> repre;
     vector<gp> smol;
-    int n;
 
-    // inicializar passando a qtd de vertices, grupos iniciais, se t = 0 entao so aloca a memoria sem inicializar
-    DSU (int n_,int t = 1) : n(n_){
-        repre.resize(n+10);
-        smol.resize(n+10);
+    // se t = 0, aloca sem init
+    DSU (int nn,int t = 1) : n(nn), repre(nn+1), smol(nn+1){
         if(t){
             for(int i=1; i<=n; i++){
                 repre[i]=i;
@@ -37,46 +25,32 @@ struct DSU{
             }
         }
     }
-    ~DSU(){
-        repre.clear();
-        smol.clear();
-    }
-    void init(int n){
-        this->n = n;
+    void init(int nn){
+        n = nn;
         for(int i=1; i<=n; i++){
             repre[i] = i;
             smol[i].init(i);
         }
     }
-    void clear(){
-        for(int i=1; i<=n; i++){
-            smol[i].clear();
-        }
-    }
 
 
-    // achar o representante do u
     int rep(int u){
         return repre[u] = (repre[u] == u ? u : rep(repre[u]));
     }
-    // pegar o smol do cara u
     gp & smoll(int u){
         return smol[rep(u)];
     }
 
     // unir u e v
     void unite(int u,int v,int t){
-        u=rep(u);
-        v=rep(v);
+        u = rep(u), v = rep(v);
         if(u == v) return;
-
         auto &x=smol[u];
         auto &y=smol[v];
         if(y.tam > x.tam){
             unite(v,u,t);
             return;
         }
-        
         // da merge nos smols
         merge(x,y,t);
         repre[v]=u;
