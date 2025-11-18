efe89fe4187a0d27c46aa9cf61932c44781c396f
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-11-18 15:41:09 -0300

    Update Convex Hull Trick.cpp

--- Data Structure/Convex Hull Trick.cpp
+++ Data Structure/Convex Hull Trick.cpp
@@ -1,3 +1,5 @@
+// Convex Hull Trick Dinamico
+// O(log2(n)) por insercao e query
 struct Line{
     // coef angular, linear, criterio de comparacao
     mutable ll k,m,p;
@@ -43,5 +45,43 @@ struct CHT : multiset<Line,less<>>{
         auto ans=lower_bound(x);
         #warning cuidado com overflow
         return ans->k*x+ans->m;
+    }
+};
+
+
+// Convex Hull Trick Estatico
+//
+// adds tem que serem feitos em ordem de slope
+// queries tem que ser feitas em ordem de x
+//
+// add O(1) amortizado, get O(1) amortizado
+struct CHT{
+	int it;
+	vector<pair<ll,ll>> cht;
+	CHT():it(0){}
+    ll f(int id, ll x){return cht[id].first*x + cht[id].second}
+    // se vc quer minimizar bota min, senao bota max
+    bool bst(ll v1, ll v2){return min(v1,v2);}
+
+    ll eval(int i, ll x){
+		return a[i]*x + b[i];
+	}
+	bool useless(){
+        int tam = cht.size();
+        int l = tam-3, mid = tam-2, r = tam-1;
+        #warning cuidado com overflow!
+		return (cht[l].second - cht[r].second)*(cht[mid].first - cht[l].first) <
+			(cht[l].second - cht[mid].second)*(cht[r].first - cht[l].first);
+	}
+    // add a*x+b
+	void add(ll a, ll b){
+		cht.emplace_back(a,b);
+        while(cht.size() >= 3 && useless()) 
+            cht.erase(cht.end()-2);
+        it = min(it, int(cht.size())-1);
+	}
+    ll query(ll x){
+        while(it+1 < cht.size() && bst(f(it+1,x),f(it,x))) it++;
+        return f(it,x);
     }
 };
