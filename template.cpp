edd9d7eda41b63fc57b589843877d75cc7b13952
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2025-12-22 20:07:38 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define rig(x) (lef(x)|1)
 #define get_bit(x,i) ((x)&(1ll<<(i)))
 #define sq(x) ((x)*(x))
+#define el '\n'
 using ll = long long int; using ull = unsigned long long int; using i28 = __int128_t; using ld = long double;
 template<class T> using vc = vector<T>; template<class T> using vvc = vector<vc<T>>;
 // const int MAX = 5e5+10;
