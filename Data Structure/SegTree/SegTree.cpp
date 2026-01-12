e2d245f64492cbd8eb2d7dd427189f119b9686f0
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-01-12 14:29:03 -0300

    Update SegTree.cpp

--- Data Structure/SegTree/SegTree.cpp
+++ Data Structure/SegTree/SegTree.cpp
@@ -3,7 +3,7 @@
 // Init: 4*N
 // Query: 4*log(N)
 // Update: 4*log(N)
-// Definir o node
+// Definir: node,upd,merge,apply,build,update
 struct Seg{
     Seg(int nn = 0, int t = 0) : n(nn), seg(nn<<2){
         if(t) build(1,1,n);
@@ -15,15 +15,15 @@ struct Seg{
     }
     // No da seg
     struct node{
+
         bool off = false;
     };
     // Update
-    struct sono{
+    struct upd{
 
     };
 
-    int n;
-    vector<node> seg;
+    int n; vector<node> seg;
     node ret,aux;
     void merge(node &x, node &y, node & at){
         if(x.off) return void(at = y);
@@ -32,15 +32,12 @@ struct Seg{
         
     }
     // aplicar um update
-    void apply(int u,int tl,int tr,sono& x){
+    void apply(int u,int tl,int tr,upd& x){
 
     }
     
     void build(int u,int tl,int tr){
-        if(tl == tr){
-            seg[u] = {};
-            return;
-        }
+        if(tl == tr) return void(seg[u] = {false});
         int tmid = tl + tr; tmid >>= 1;
         build(lef(u), tl, tmid), build(rig(u), tmid+1, tr);
         merge(seg[lef(u)], seg[rig(u)], seg[u]);
@@ -48,10 +45,7 @@ struct Seg{
     
     void query(int u,int tl,int tr,int l, int r){
         if(l > r) return;
-        if(tl == l && tr == r) {
-            merge(aux = ret,seg[u],ret);
-            return;
-        }
+        if(tl == l && tr == r) return merge(aux = ret, seg[u], ret);
         int tmid= tl + tr; tmid >>= 1;
         query(lef(u),tl,tmid,l,min(tmid,r)), query(rig(u),tmid+1,tr,max(tmid+1,l),r);   
     }
@@ -61,19 +55,16 @@ struct Seg{
         return ret;
     }
     
-    void update(int u, int tl, int tr, int id, sono& x){
-        if(tl == tr){
-            apply(u,tl,tr,x);
-            return;
-        }
+    void update(int u, int tl, int tr, int id, upd& x){
+        if(tl == tr) return apply(u,tl,tr,x);
         int tmid = tl + tr; tmid >>= 1;
         if(tmid >= id) update(lef(u),tl,tmid,id,x);
         else update(rig(u),tmid+1,tr,id,x);
         merge(seg[lef(u)], seg[rig(u)], seg[u]);
     }
-    // passa os parametros que dai vai converter pra sono
+    // passa os parametros que dai vai converter pra upd
     void update(int id, ll x){
-        sono vals = {};
+        upd vals = {};
         update(1, 1, n, id, vals);
     }
 };
