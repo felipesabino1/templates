02b59657ad6340a19d3443dc680d9b2a7fe35802
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-02-27 18:34:26 -0300

    Refactor interval constructor and insertion logic

--- Data Structure/Interval Manager.cpp
+++ Data Structure/Interval Manager.cpp
@@ -7,7 +7,7 @@ template <class TT = int>
 struct STI{
     struct interval{
         TT l,r;
-        interval(TT l,TT r) : l(l),r(r){}
+        interval(TT a,TT b) : l(a),r(b){}
         interval(TT x) : l(x),r(x){}
 
         bool operator <(const interval & ot)const{
@@ -19,36 +19,32 @@ struct STI{
     set<interval> st;
     TT qtd = 0; // qtd de pontos
     const TT off_l = -1; // o L mais a esquerda, pra nao existir 
+    // tipo do merge, 1: (a,b)+(b+1,c) = (a,c); 0: (a,b)+(b+1,c) = (a,b) e (b+1,c)
+    bool t;
 
+    STI(bool tt = false) : t(tt){}
     void clear(){
         qtd = 0;
         st.clear();
     }
 
     void insert(TT l, TT r){
+        if(l > r) return;
         // arrumando os caras que me pega por tras
         auto it = st.lower_bound(interval(l,off_l));
         while(!st.empty() && it != st.begin()){
             it--;
-            // (l1-r1),(r1+1-r2) -> (l1-r2)
-            // if(it->r < l-1) break;
-            // (l1-r1),(r+1-r2) -> (l1-r1),(r+1-r2)
-            if(it->r < l) break;
+            if(it->r < l-(t ? 1 : 0)) break;
             qtd -= it->r - it->l + 1;
-            l = min(it->l,l);
-            r = max(it->r,r);
+            l = min(it->l,l), r = max(it->r,r);
             it = st.erase(it);
         }
         // arrumando os caras que eu pego
         it = st.lower_bound(interval(l,off_l));
         while(!st.empty() && it != st.end()){
-            // (l1-r1),(r1+1-r2) -> (l1-r2)
-            // if(it->l > r+1) break;
-            // (l1-r1),(r+1-r2) -> (l1-r1),(r+1-r2)
-            if(it->l > r) break;
+            if(it->l > r+(t ? 1 : 0)) break;
             qtd -= it->r - it->l + 1;
-            l = min(it->l,l);
-            r = max(it->r,r);
+            l = min(it->l,l), r = max(it->r,r);
             it = st.erase(it);
         }
         qtd += r - l + 1;
