9b7cf48ad8946cae8fb44575323cd03bce2e1b1a
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-02-28 17:57:42 -0300

    Refactor DSU structure and simplify merge logic

--- Data Structure/DSU-SMOL.cpp
+++ Data Structure/DSU-SMOL.cpp
@@ -1,61 +1,46 @@
 // Indexado em 1
-// Alterar o que cada grupo tem
+// Definir o que gp carrega de informacao
+// Definir o merge dos grupos
+// O(N * log(N) * T) com T a complexidade do merge
 struct DSU{
     // o que cada grupo carrega de informacao
     struct gp{
-        int tam = 0;
         void init(int x){
-            tam=1;
         }
         void clear(){
-            tam = 0;
         }
     };
     
     int n;
-    vector<int> rep;
+    vector<int> rep,tam;
     vector<gp> smol;
 
-    // se t = 0, aloca sem init
-    DSU (int nn,int t = 1) : n(nn), rep(nn+1), smol(nn+1){
-        if(t){
-            for(int i=1; i<=n; i++){
-                rep[i]=i;
-                smol[i].init(i);
-            }
-        }
+    // se t = false, aloca sem init
+    DSU (int nn,bool t = true) : n(nn),rep(nn+1),tam(nn+1),smol(nn+1){
+        if(t) init(nn);
     }
     void init(int nn){
         n = nn;
         for(int i=1; i<=n; i++){
-            rep[i] = i;
+            rep[i] = i,tam[i]=1;
             smol[i].init(i);
         }
     }
 
-    int get(int u){
-        return rep[u] = (rep[u] == u ? u : get(rep[u]));
-    }
-    gp & smoll(int u){
-        return smol[get(u)];
-    }
+    int get(int u){return rep[u] = (rep[u] == u ? u : get(rep[u]));}
+    gp& smoll(int u){return smol[get(u)];}
 
     // unir u e v
     void unite(int u,int v,int t = 0){
         u = get(u), v = get(v);
         if(u == v) return;
-        auto &x=smol[u];
-        auto &y=smol[v];
-        if(y.tam > x.tam) return void(unite(v,u,t));
-        // da merge nos smols
-        merge(x,y,t);
-        rep[v]=u;
+        if(tam[u] < tam[v]) swap(u,v);
+        merge(smoll(u),smoll(v),t);
+        rep[v]=u,tam[u] += tam[v];
     }
 
     // fazer o merge de 2 grupos
     void merge(gp &x, gp &y,int t){
-        // faz o merge se precisar
-        x.tam += y.tam;
         y.clear();
     }
 };
