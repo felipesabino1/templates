30940e576ab9840c0ddc002b54d0497138eb37f2
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-03-05 13:40:13 -0300

    Refactor SlopeTrick to use priority_queue
    Refactor SlopeTrick structure to use priority_queue and simplify add method.

--- Data Structure/Slope Trick.cpp
+++ Data Structure/Slope Trick.cpp
@@ -1,29 +1,19 @@
 // Armazena funções slope-trickables, que sao funcoes que seguem as regras:
 // Sao continuas, podem ser divididas em diversas secoes tal que todas as lineares com slopes inteiros e
 // sao concavas/convexas
-  struct SlopeTrick{
+struct SlopeTrick{
     struct func{
         ll a,b;
         func(ll aa = 0, ll bb = 0) : a(aa), b(bb){}
         friend ostream& operator<<(ostream& out, func f){return out << f.a << "x " << (f.b < 0 ? '-' : '+') << abs(f.b);}
+        void operator+=(func ot){a += ot.a, b += ot.b;}
     };
-    multiset<pair<ll,ll>> pts; // slope change (pos,qtd)
-    func f; // left,right,mid linear func
+    priority_queue<pair<ll,ll>> pts; // slope change (pos,qtd)
+    func f; // right linear func
 
-    void add(ll x, ll qt){pts.emplace(x,qt);}
-    void show(){
-        auto st = pts;
-        auto mf = f;
-        while(!st.empty()){
-            auto [x,qt] = *st.begin(); st.erase(st.begin());
-            cout << mf << el;
-            cout << "x: " << x << el;
-            ll y = func.a*x + func.b;
-            func.a -= qt;
-            func.b = y - func.a*x;
-        }
-        cout << mf << el;
-        cout << "x: inf" << el;            
+    void add(ll X){
+        pts.emplace(X,2);
+        f += func(1,-X);
     }
     ll bst(){
         return f.b;
