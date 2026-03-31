3a6254ca46fd586bf7a82f69c8e1d657d10e7f50
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-03-31 14:59:52 -0300

    Update Persistent Stack.cpp

--- Data Structure/Persistent/Persistent Stack.cpp
+++ Data Structure/Persistent/Persistent Stack.cpp
@@ -1,15 +1,16 @@
 // Revisoes da stack indexadas de 0
 // Push e pop: O(1)
+struct info{
+    
+};
+template<class T>
 struct perstack{
-    struct info{
-
-    };
     struct node{
-        info val;int prox = 0;
+        T val;int prox = 0;
         node(info vall,int proxx) : val(vall),prox(proxx){}
     };
     vc<node> vals; // nodes
     vc<int> rev; // topo da revisao
-    void push(info x, int r=-1){rev.push_back(st.size()), st.emplace_back(x,r);}
+    void push(T x, int r=-1){rev.push_back(st.size()), st.emplace_back(x,r);}
     void pop(int r){rev.push_back(vals[rev[r]].prox);}
 };
