5fd2e55eedd71cd50cda84c1f78a58bcf35e5f60
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-04-06 16:02:15 -0300

    Update Fractions.cpp

--- Math/Fractions.cpp
+++ Math/Fractions.cpp
@@ -1,49 +1,42 @@
 template <class TT = ll>
 struct frac{
     TT num,den; // o valor negativo da fracao fica so no numerador
- 
-    frac(TT num_ = 0,TT den_ = 1) : num(num_), den(den_){
+    frac(TT numm = 0,TT denn = 1) : num(numm), den(denn){
         TT g = __gcd(abs(num),abs(den));
-        num/=g; den/=g;
-        if((num^den) < 0) num = -abs(num);
-        den = abs(den);
+        num/=g, den/=g; 
+        num = ((num^den) < 0 ? -1 : 1) * abs(num); den = abs(den);
     }
-    friend istream& operator>>(istream& in, frac &at){return in >> at.num;}
     friend ostream &operator<<(ostream & out, const frac at){
         return out << at.num << '/' << at.den;
     }
- 
     explicit operator int()const{return num/den;}
     explicit operator double()const{return double(num)/den;}
-    explicit operator long double()const{return ((long double)num)/den;}
     bool operator<(const frac& ot)const{return 1ll*num*ot.den < 1ll*ot.num*den;}
     bool operator>(const frac& ot)const{return 1ll*num*ot.den > 1ll*ot.num*den;}
     bool operator<=(const frac &ot)const{return 1ll*num*ot.den <= 1ll*ot.num*den;}
     bool operator>=(const frac &ot)const{return 1ll*num*ot.den >= 1ll*ot.num*den;}
     bool operator==(const frac& ot)const{return num == ot.num && den == ot.den;}
-    
+
     frac operator-(){return frac(-num,den);}
-    friend frac inv(frac f){return frac(f.den,f.num);}
-    
+    friend frac inv(frac f){return frac(f.den,f.num);}    
     friend frac& operator+=(frac& a, frac b){
         TT d = a.den/__gcd(a.den,b.den)*b.den;
-        a.num = d/a.den*a.num + d/b.den*b.num;
-        a.den = d;
+        a.num *= d/a.den, b.num *= d/b.den;
+        a.num += b.num, a.den = d;
         return a;
     }
     friend frac& operator-=(frac& a, frac b){
         return a += -b;
     }
     friend frac& operator*=(frac& a, frac b){
-        TT d = __gcd(a.num,b.den); a.num /= d; b.den /= d;
-        d = __gcd(a.den,b.num); a.den /= d; b.num /= d;
-        a.num *= b.num; a.den *= b.den;
+        TT d = __gcd(a.num,b.den); a.num /= d, b.den /= d;
+        d = __gcd(a.den,b.num); a.den /= d, b.num /= d;
+        a.num *= b.num, a.den *= b.den;
         return a;
     }
     friend frac& operator/=(frac& a, frac b){
         return a *= inv(b);
     }
-
     friend frac operator+(frac a, frac b){return a += b;}
     friend frac operator-(frac a, frac b){return a -= b;}
     friend frac operator*(frac a, frac b){return a *= b;}
