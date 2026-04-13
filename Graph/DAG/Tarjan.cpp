3ca231dbe4deda2e6bf92672b9b423c68ec74d8e
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-04-13 18:13:12 -0300

    Update Tarjan.cpp

--- Graph/DAG/Tarjan.cpp
+++ Graph/DAG/Tarjan.cpp
@@ -7,18 +7,18 @@ struct Tarjan{
     vvc<int> ng;
     Tarjan(vvc<int>& g) : graph(g), tin(g.size(),0), low(g.size(),0), root(g.size(),-1){
         int n = graph.size();
-        for(int i=0; i<n; i++) if(!tin[i]) dfs(i,-1);
+        for(int i=0; i<n; i++) if(!tin[i]) dfs(i);
         for(int i=0; i<n; i++){
             int u = root[i];
             for(auto v : graph[i]) if(u != root[v]) ng[u].push_back(root[v]);
         }
         tin.clear(), low.clear(), root.clear(), st.clear();
     } 
-    void dfs(int u,int ant){
+    void dfs(int u){
         tin[u] = low[u] = ++timer;
         st.push_back(u);
         for(auto v : graph[u]){
-            if(!tin[v]) dfs(v,u),low[u] = min(low[u],low[v]);
+            if(!tin[v]) dfs(v),low[u] = min(low[u],low[v]);
             else if(root[v] == -1) low[u] = min(low[u],tin[v]);
         }
         if(tin[u] == low[u]){
