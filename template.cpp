ebb9dce3a42d29bc3fa2c6cafdea36fd33cf35b9
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-04-15 17:22:44 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,16 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define lef(x) (x<<1)
-#define rig(x) (lef(x)|1)
 #define get_bit(x,i) ((x)&(1ll<<(i)))
 #define sq(x) ((x)*(x))
 #define el '\n'
 using ll = long long int; using ull = unsigned long long int; using i28 = __int128_t; using ld = long double;
 template<class T> using vc = vector<T>; template<class T> using vvc = vector<vc<T>>;
-// const int MAX = 5e5+10;
-// const int mod = 1e9 + 7; // 998244353;
-// const int inf = 2'000'000'000;
-// const int TETO = 20;
  
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
