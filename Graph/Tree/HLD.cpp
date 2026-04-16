bb755bf036b60d542344c76dc494602653cc9634
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-04-16 09:24:08 -0300

    Update HLD.cpp

--- Graph/Tree/HLD.cpp
+++ Graph/Tree/HLD.cpp
@@ -1,42 +1,39 @@
-// Indexado de 1
-// Query de op associativas e updt sobre paths 
-// Init - 8*N
-// Query - 4*log^2(N) (vai ser melhor que isso)
-// Update - 4*log^2(N) (vai ser melhor que isso)
-// Se a op eh simetrica da pra ignorar o iseg e fazer so com o seg (iseg.query(inv(l),inv(r)) -> seg.query(dir(r),dir(l)))
+// Indexado de 0
+// Query de op associativas e updt sobre paths e subtrees
+// Init(8*N), Query(4*log^2(N)), Update(4*log^2(N))
+// Se op eh simetrico da pra apagar iseg e substituir todos os (iseg.query(inv(l),inv(r)) -> seg.query(dir(r),dir(l)))
+// struct Seg{...}
+template <class node, class sono>
 struct HLD{
-    // struct Seg{...}
-    int n,timer; bool sim;
-    vector<int> tam,tin,hd,p,lst;
-    #warning definir o tipo do euler
-    vector<T> euler; 
-    Seg seg,iseg;
-    void dfs(int u,int ant=-1, int f=-1){
-        tam[u] = 1, euler[tin[u] = ++timer] = VEC[u];
+    int n,timer;
+    vector<int> tam,tin,hd,p;
+    vector<node> euler; vector<node> &vec; 
+    Seg<node,sono> seg,iseg;
+    void dfs(int u,int ant=-1, int f=1){
+        tam[u] = 1, euler[tin[u] = timer++] = vec[u];
         p[u] = (ant == -1 ? u : ant), hd[u] = (ant == -1 ? u : hd[u]);
         for(auto& v : g[u]) if(v != ant){
             hd[v] = (v == g[u][0] ? hd[u] : v);
             dfs(v,u,f); tam[u] += tam[v];
             if(tam[v] > tam[g[u][0]] || g[u][0] == ant) swap(v,g[u][0]);
         }
-        if (ant*f == -1) build_hld(hd[u]=u,-1,timer=0);
+        if (ant*f == -1) dfs(hd[u]=u,-1,timer=0);
     }
-    HLD(int nn,int r=1) : n(nn),timer(0),tam(nn+1),tin(nn+1),hd(nn+1),p(nn+1),lst(nn+1,0),euler(nn+1){
-        dfs(r,r);
-        seg.init(nn,euler); 
+    HLD(int nn,vc<node> &v,int r=0) : n(nn),timer(0),tam(nn),tin(nn),hd(nn),p(nn),euler(nn),vec(v){
+        dfs(r);
+        seg.init(n,euler); 
         // se for comutativo nao precisa
-        reverse(euler.begin()+1,euler.end());
-        iseg.init(nn,euler);
-        reverse(euler.begin()+1,euler.end());
+        reverse(euler.begin(),euler.end());
+        iseg.init(n,euler);
+        reverse(euler.begin(),euler.end());
     }
     // u eh lca de v?
     bool lca(int u, int v){return tin[u] <= tin[v] && tin[v]+tam[v]-1 <= tin[u]+tam[u]-1;}
-    #define dir(u) tin[u]
-    #define inv(u) n-tin[u]+1
-    #define merge seg.merge
+    #define dir(u) (tin[u])
+    #define inv(u) (n-1-tin[u])
     // query no path de u->v
-    Seg::node query_path(int u,int v){
-        seg::node a1,a2,ru,rv;
+    node query_path(int u,int v){
+        node a1,a2,ru,rv;
         a1.off = a2.off = ru.off = rv.off = true;
         for(; !lca(hd[u],v); u=p[hd[u]])
             merge(a1 = ru, a2 = iseg.query(inv(u),inv(hd[u])), ru);
@@ -61,7 +58,6 @@ struct HLD{
     }
     #undef dir
     #undef inv
-    #undef merge
-    Seg::node query_tree(int u){return seg.query(tin[u],tin[u]+tam[u]-1);}
-    void update_tree(int u){seg.update(tin[u],tin[u]+tam[u]-1);}
+    node query_tree(int u){return seg.query(tin[u],tin[u]+tam[u]-1);}
+    void update_tree(int u, sono x){seg.update(tin[u],tin[u]+tam[u]-1,x);}
 };
