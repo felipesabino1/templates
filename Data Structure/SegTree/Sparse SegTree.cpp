114a230a631ae23aac2086ee799ed45c1b9e01ce
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-06-01 18:53:08 -0300

    Update Sparse SegTree.cpp

--- Data Structure/SegTree/Sparse SegTree.cpp
+++ Data Structure/SegTree/Sparse SegTree.cpp
@@ -3,10 +3,11 @@
 // Query: 4*log(N)
 // Update: 4*log(N)
 // Q*log(N) de memoria
-// Definir tudo que ta fora de Seg
+// Definir tudo que ta fora de Seg, cuidado quando o retorno da query estiver off
 struct node{
     
-    bool off = true; int l = 0,r = 0;
+    bool off = true; 
+    int l = 0,r = 0; ll tl,tr; // ponteiros, range do node
     void merge(node &x,node &y,node &at){
         if(x.off) return void(at = y); // as vezes mudar o que o fazer com o off
         if(y.off) return void(at = x);
@@ -32,7 +33,9 @@ template <class node, class upd>
 struct Seg{
     #define lef(x) seg[x].l
     #define rig(x) seg[x].r
-    Seg(int nn = 0) : n(nn),seg(1),lazy(1){}
+    Seg(int nn = 0) : n(nn),seg(1),lazy(1){
+        seg[0].tl = 0, seg[0].tr = n-1;
+    }
     int n; vc<node> seg;  vc<upd> lazy;
     node ret,aux,offn;
     void push(int u,int tl,int tr){
@@ -49,20 +52,21 @@ struct Seg{
     void query(int u,int tl,int tr,int l, int r){
         if(l > r) return;
         if(l == tl && tr == r) return merge(aux = ret,seg[u],ret);
-        if(!lef(u)) lef(u) = add(), rig(u) = add();
         push(u,tl,tr); int tmid = tl + tr; tmid >>= 1;
+        if(!lef(u)) lef(u) = add(), lef(u).tl = tl, lef(u).tr = tmid;
+        if(!rig(u)) rig(u) = add(), rig(u).tl = tmid+1, rig(u).tr = tr;
         query(lef(u),tl,tmid,l,min(r,tmid)),query(rig(u),tmid+1,tr,max(l,tmid+1),r);
     }
     node query(int l, int r){
         ret.off = true; query(0,0,n-1,l,r);
-        if(ret.off) // off val
-        return ret;
+        return ret; // vc que trata quando o ret.off == true
     }
     void update(int u, int tl, int tr, int l, int r, upd& x){
         if(l > r) return;
         if(l == tl && tr == r) return apply(seg[u],lazy[u],x);
-        if(!lef(u)) lef(u) = add(), rig(u) = add();
         push(u,tl,tr); int tmid = tl + tr; tmid >>= 1;
+        if(!lef(u)) lef(u) = add(), lef(u).tl = tl, lef(u).tr = tmid;
+        if(!rig(u)) rig(u) = add(), rig(u).tl = tmid+1, rig(u).tr = tr;
         update(lef(u),tl,tmid,l,min(r,tmid),x),update(rig(u),tmid+1,tr,max(l,tmid+1),r,x);
         merge(seg[lef(u)],seg[rig(u)],seg[u]);
     }
