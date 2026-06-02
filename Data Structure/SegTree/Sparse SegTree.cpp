ade600ca2c859f372eebee9b9027e8d17b639e0f
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-06-01 22:29:14 -0300

    Update Sparse SegTree.cpp

--- Data Structure/SegTree/Sparse SegTree.cpp
+++ Data Structure/SegTree/Sparse SegTree.cpp
@@ -5,11 +5,13 @@
 // Q*log(N) de memoria
 // Definir tudo que ta fora de Seg, cuidado quando o retorno da query estiver off
 // Nao precisa atualizar as ranges no merge, eu faco isso quando crio o node
+template<class T>
 struct node{
-    
-    bool off = true; 
-    int l = 0,r = 0; ll tl,tr; // ponteiros, range do node
-    void merge(node &x,node &y,node &at){
+
+    bool off = true;
+    int l = 0,r = 0; T tl,tr; // ponteiros, range do node
+    node(){}
+    friend void merge(node<T> &x,node<T> &y,node<T> &at){
         if(x.off) return void(at = y); // as vezes mudar o que o fazer com o off
         if(y.off) return void(at = x);
         // o at eh o merge do x(esq) e y(dir)
@@ -17,11 +19,13 @@ struct node{
         at.off = false;
     }
 };
+template<class T>
 struct upd{
 
-    bool off = true; 
+    bool off = true;
+    upd(){}
     // aplica upd e upd lazy
-    friend void apply(node &at,upd &lazy,upd &x){
+    friend void apply(node<T> &at,upd<T> &lazy,upd<T> &x){
         if(at.off) // init
         // updt seg
         at.off = false;
@@ -30,18 +34,18 @@ struct upd{
         lazy.off = false;
     }
 };
-template <class node, class upd>
+template <class node, class upd, class T>
 struct Seg{
     #define lef(x) seg[x].l
     #define rig(x) seg[x].r
-    Seg(int nn = 0) : n(nn),seg(1),lazy(1){
+    Seg(T nn = 0) : n(nn),seg(1),lazy(1){
         seg[0].tl = 0, seg[0].tr = n-1;
     }
-    int n; vc<node> seg;  vc<upd> lazy;
+    T n; vc<node> seg;  vc<upd> lazy;
     node ret,aux,offn;
-    void push(int u,int tl,int tr){
+    void push(int u,T tl,T tr){
         if(tl == tr || lazy[u].off) return;
-        int tmid = tl + tr; tmid >>= 1;
+        T tmid = tl + tr; tmid >>= 1;
         apply(seg[lef(u)],lazy[lef(u)],lazy[u]),apply(seg[rig(u)],lazy[rig(u)],lazy[u]);
         lazy[u].off = true;
     }
@@ -50,30 +54,30 @@ struct Seg{
         seg.emplace_back(),lazy.emplace_back();
         return x;
     }
-    void query(int u,int tl,int tr,int l, int r){
+    void query(int u,T tl,T tr,T l, T r){
         if(l > r) return;
         if(l == tl && tr == r) return merge(aux = ret,seg[u],ret);
-        int tmid = tl + tr; tmid >>= 1;
-        if(!lef(u)) lef(u) = add(), lef(u).tl = tl, lef(u).tr = tmid;
-        if(!rig(u)) rig(u) = add(), rig(u).tl = tmid+1, rig(u).tr = tr;
-        push(u,tl,tr); 
+        T tmid = tl + tr; tmid >>= 1;
+        if(!lef(u)) lef(u) = add(), seg[lef(u)].tl = tl, seg[lef(u)].tr = tmid;
+        if(!rig(u)) rig(u) = add(), seg[rig(u)].tl = tmid+1, seg[rig(u)].tr = tr;
+        push(u,tl,tr);
         query(lef(u),tl,tmid,l,min(r,tmid)),query(rig(u),tmid+1,tr,max(l,tmid+1),r);
     }
-    node query(int l, int r){
+    node query(T l, T r){
         ret.off = true; query(0,0,n-1,l,r);
         return ret; // vc que trata quando o ret.off == true
     }
-    void update(int u, int tl, int tr, int l, int r, upd& x){
+    void update(int u, T tl, T tr, T l, T r, upd& x){
         if(l > r) return;
         if(l == tl && tr == r) return apply(seg[u],lazy[u],x);
-        int tmid = tl + tr; tmid >>= 1;
-        if(!lef(u)) lef(u) = add(), lef(u).tl = tl, lef(u).tr = tmid;
-        if(!rig(u)) rig(u) = add(), rig(u).tl = tmid+1, rig(u).tr = tr;
+        T tmid = tl + tr; tmid >>= 1;
+        if(!lef(u)) lef(u) = add(), seg[lef(u)].tl = tl, seg[lef(u)].tr = tmid;
+        if(!rig(u)) rig(u) = add(), seg[rig(u)].tl = tmid+1, seg[rig(u)].tr = tr;
         push(u,tl,tr);
         update(lef(u),tl,tmid,l,min(r,tmid),x),update(rig(u),tmid+1,tr,max(l,tmid+1),r,x);
         merge(seg[lef(u)],seg[rig(u)],seg[u]);
     }
-    void update(int l, int r, upd x){update(0,0,n-1,l,r,x);} // [l,r]
+    void update(T l, T r, upd x){update(0,0,n-1,l,r,x);} // [l,r]
     #undef lef
     #undef rig
 };
