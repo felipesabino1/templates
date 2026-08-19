c7ce9336d2f9a78b12890b65360cecee6171caa3
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-08-19 12:39:41 -0300

    Update Interval Manager.cpp

--- Data Structure/Interval Manager.cpp
+++ Data Structure/Interval Manager.cpp
@@ -1,13 +1,9 @@
-/*
-    Alteracoes:
-
-    N hora de dar insert nos intervalos, ver qual tipo de merge que deve considerar l-r r+1-r2 vira l-r2 ou se nao.
-*/
+// Verificar no insert de intervalos qual tipo de merge que deve considerar [l,r]\cup[r+1,r2] vira [l,r2] ou se nao.
 template <class TT = int>
 struct STI{
     struct interval{
         TT l,r;
-        interval(TT a,TT b) : l(a),r(b){}
+        interval(TT l,TT r) : l(l),r(r){}
         interval(TT x) : l(x),r(x){}
 
         bool operator <(const interval & ot)const{
@@ -19,32 +15,42 @@ struct STI{
     set<interval> st;
     TT qtd = 0; // qtd de pontos
     const TT off_l = -1; // o L mais a esquerda, pra nao existir 
-    // tipo do merge, 1: (a,b)+(b+1,c) = (a,c); 0: (a,b)+(b+1,c) = (a,b) e (b+1,c)
-    bool t;
 
-    STI(bool tt = false) : t(tt){}
     void clear(){
         qtd = 0;
         st.clear();
     }
 
     void insert(TT l, TT r){
-        if(l > r) return;
         // arrumando os caras que me pega por tras
         auto it = st.lower_bound(interval(l,off_l));
         while(!st.empty() && it != st.begin()){
             it--;
-            if(it->r < l-(t ? 1 : 0)) break;
+            #warning escolher qual o merge certo
+            // [l1,r1]\cup[r1+1,r2] == (l1,r2)
+            // if(it->r < l-1) break;
+
+            // [l1,r1]\cup[r1+1,r2] == [l1,r1]\cup[r1+1,r2]
+            if(it->r < l) break;
+            
             qtd -= it->r - it->l + 1;
-            l = min(it->l,l), r = max(it->r,r);
+            l = min(it->l,l);
+            r = max(it->r,r);
             it = st.erase(it);
         }
         // arrumando os caras que eu pego
         it = st.lower_bound(interval(l,off_l));
         while(!st.empty() && it != st.end()){
-            if(it->l > r+(t ? 1 : 0)) break;
+            #warning escolher qual o merge certo
+            // [l1,r1]\cup[r1+1,r2] == [l1,r2]
+            // if(it->l > r+1) break;
+
+            // [l1,r1]\cup[r1+1,r2] == [l1,r1]\cup[r1+1,r2]
+            if(it->l > r) break;
+            
             qtd -= it->r - it->l + 1;
-            l = min(it->l,l), r = max(it->r,r);
+            l = min(it->l,l);
+            r = max(it->r,r);
             it = st.erase(it);
         }
         qtd += r - l + 1;
