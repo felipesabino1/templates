f40df29b195e47ccc3f4c5f7291d93360aae2a56
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-08-25 15:20:34 -0300

    Update Linear System.cpp

--- Math/Linear System.cpp
+++ Math/Linear System.cpp
@@ -1,13 +1,13 @@
 // Resolve sistema linear
-// Se for usar double ou modint eh so mudar o TT
+// Se for usar double ou modint eh so mudar o T
 // Retorna uma solucao, caso exista, e outros vetores para vc alterar a solucao base
 // O Rank vai ser o tamanho do vetor de retorno -1
 // O(N^2 * M)
 namespace Gauss{
     const double eps = 1e-9;
-    using TT = double; // modint
+    using T = double; // modint
 
-    void show(vvc<TT>& a, vc<TT>& b){
+    void show(vvc<T>& a, vc<T>& b){
         if(a.size() == 0) return;
         int n = a.size(), m = a[0].size();
         for(int i = 0; i < n; i++){
@@ -15,12 +15,12 @@ namespace Gauss{
             cout << "= " << b[i] << '\n';
         }
     }
-    bool zero(TT x){ // x == 0?
+    bool zero(T x){ // x == 0?
         return fabs(x) < eps;
         // return !int(x.v);
     }
 
-    vvc<TT> solveLinear(vvc<TT> a, vc<TT> b){
+    vvc<T> solveLinear(vvc<T> a, vc<T> b){
         if(a.empty() || a[0].empty()) return {};
         int n = a.size(), m = a[0].size(), r = 0;
         assert(int(b.size()) == n);
@@ -34,7 +34,7 @@ namespace Gauss{
             if(lin == -1) continue; // so tem zero na coluna, motivo de existir idcol
             if(r != lin) swap(a[r],a[lin]); // arrumar a diagonal
             swap(b[r],b[lin]);
-            TT ival = 1/a[r][j];
+            T ival = 1/a[r][j];
             for(int i = r+1; i < n; i++){
                 if(zero(a[i][j])) continue;
                 auto freq = a[i][j] * ival;
@@ -53,10 +53,10 @@ namespace Gauss{
         // C = sum(V_i * c_i), C eh uma combinacao linear qualquer de V
         // Solucao_qualquer = X0 + C
         // sols = {X0,V1,V2,V3,...,V_r}
-        vvc<TT> sols;
+        vvc<T> sols;
         // solution X0
         {
-            vc<TT> v(m,0);
+            vc<T> v(m,0);
             for(int i=r-1; i >= 0; i--){
                 int id = idcol[i];
                 v[id] = b[i];
@@ -68,7 +68,7 @@ namespace Gauss{
         // free variables
         for(int s=0; s<m; s++){
             if(used[s]) continue;
-            vc<TT> v(m,0);
+            vc<T> v(m,0);
             v[s] = 1;
             for(int i=r-1; i>=0; i--){
                 int id = idcol[i];
