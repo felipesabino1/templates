46cc6de9d2d63abbb30c0ce75be0b08178e9f36a
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-01 10:39:34 -0300

    Update Xor Basis.cpp

--- Math/Xor Basis.cpp
+++ Math/Xor Basis.cpp
@@ -1,22 +1,22 @@
 // Add(TAM), Has(TAM)
-template<class TT = ll>
+template<class T = ll>
 struct Basis{
-    vector<TT> basis;
+    vector<T> b;
     int tam;
-    Basis(int tamm) : basis(tamm,0), tam(tamm){}
-    void add(TT x){
+    Basis(int tamm) : b(tamm,0), tam(tamm){}
+    void add(T x){
         for(int i=tam-1; i>=0; i--) if(get_bit(x,i)){
-            if(!basis[i]) return void(basis[i] = x);
-            else x ^= basis[i];
-            // as vezes vc vai ter uma outra condicao ao inves desse else
-            // if(time[i] > tx) swap(time[i],tx), swap(x,basis[i])
-            // else x ^= basis[i]
+            if(!b[i]) return void(b[i] = x);
+            x ^= b[i];
+            // as vezes vc vai ter uma outra condicao ao inves de so x^=basis[i]
+            // if(time[i] > tx) swap(time[i],tx), swap(b[i],x)
+            // x ^= b[i]
         }
     }
     bool has(TT x){
         for(int i=tam-1; i>=0; i--) if(get_bit(x,i)){
-            if(!basis[i]) return false;
-            else x ^= basis[i];
+            if(!b[i]) return false;
+            x ^= b[i];
         }
         return true;
     }
