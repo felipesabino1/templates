44d598d4855a5aa95575d199fc245bcd5e98bd45
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-01 09:55:09 -0300

    Refactor Basis constructor and add method

--- Math/Xor Basis.cpp
+++ Math/Xor Basis.cpp
@@ -1,23 +1,22 @@
+// Add(TAM), Has(TAM)
 template<class TT = ll>
 struct Basis{
     vector<TT> basis;
     int tam;
-    Basis(int tam_) : basis(tam_,0), tam(tam_){}
+    Basis(int tamm) : basis(tamm,0), tam(tamm){}
     void add(TT x){
-        for(int i=tam-1; i>=0; i--){
-            if(!get_bit(x,i)) continue;
-            if(basis[i]) x ^= basis[i];
-            else{
-                basis[i] = x;
-                return;
-            }
+        for(int i=tam-1; i>=0; i--) if(get_bit(x,i)){
+            if(!basis[i]) return void(basis[i] = x);
+            else x ^= basis[i];
+            // as vezes vc vai ter uma outra condicao ao inves desse else
+            // if(time[i] > tx) swap(time[i],tx), swap(x,basis[i])
+            // else x ^= basis[i]
         }
     }
     bool has(TT x){
-        for(int i=tam-1; i>=0; i--){
-            if(!get_bit(x,i)) continue;
+        for(int i=tam-1; i>=0; i--) if(get_bit(x,i)){
             if(!basis[i]) return false;
-            x ^= basis[i];
+            else x ^= basis[i];
         }
         return true;
     }
