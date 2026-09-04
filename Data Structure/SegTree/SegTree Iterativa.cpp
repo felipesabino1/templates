73983d503dfd992cfa351a36fa7ac91c31e84c83
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-04 15:16:56 -0300

    Update SegTree Iterativa.cpp

--- Data Structure/SegTree/SegTree Iterativa.cpp
+++ Data Structure/SegTree/SegTree Iterativa.cpp
@@ -1,13 +1,11 @@
 // Indexado de 0
 // Query com op associativa e update em ponto
 // Init(N*(1 + O(merge))), Query(2*log(N)*O(merge)), Update(log(N)*O(merge))
+// Inicializar o node com valor identidade
 struct node{
     
-    bool off = false;
     node(){}
     friend void merge(node &x, node &y, node &at){
-        if(x.off) return void(at = y);
-        if(y.off) return void(at = x);
         // o at eh o merge do x(esq) e y(dir)
 
     }
@@ -34,16 +32,16 @@ struct Seg{
     }
     // query inclusiva [l,r]
     node query(int l,int r){
+        assert(0 <= l && l <= r && r < n);
         node vl,vr,aux;
-        vl.off = vr.off = true;
-        if(l > r) return vl;
         for(l+=n,r+=n+1;l<r;l>>=1,r>>=1){
-            if (l&1) merge(aux=vl,seg[l++],vl);
-            if (r&1) merge(seg[--r],aux=vr,vr);
+            if(l&1) merge(aux=vl,seg[l++],vl);
+            if(r&1) merge(seg[--r],aux=vr,vr);
         } 
         merge(vl,vr,aux); return aux;
     }
     void update(int id,upd x){
+        assert(0 <= id && id < n);
         id += n; apply(seg[id],x); 
         for(id>>=1; id; id>>=1) merge(seg[lef(id)],seg[rig(id)],seg[id]);
     }
