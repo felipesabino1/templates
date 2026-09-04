35c650b357b69a6750e658cb2aabd292ec8a41a1
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-04 11:40:59 -0300

    Update SegTree Iterativa.cpp

--- Data Structure/SegTree/SegTree Iterativa.cpp
+++ Data Structure/SegTree/SegTree Iterativa.cpp
@@ -1,9 +1,6 @@
 // Indexado de 0
 // Query com op associativa e update em ponto
-// Init: 2*N
-// Query: 2*log(N)*O(merge)
-// Update: log(N)*O(merge)
-// Definir tudo que ta fora de Seg
+// Init(2*N), Query(2*log(N)*O(merge)), Update(log(N)*O(merge))
 struct node{
     
     bool off = false;
@@ -27,10 +24,10 @@ template<class node,class upd>
 struct Seg{
     #define lef(x) ((x)<<1)
     #define rig(x) (lef(x)|1)
-    Seg(int nn = 0, vc<node> &v = {}) : n(nn), seg(nn<<1){
+    int n; vector<node> seg;
+    Seg(int nn = 0, vc<node> v = {}) : n(nn), seg(nn<<1){
         if(!v.empty()) build(v);
     }
-    int n; vector<node> seg;
     void build(vc<node>& v){
         for(int u=0; u<n; u++) seg[u+n] = v[u];
         for(int u=n-1; u; u--) merge(seg[lef(u)],seg[rig(u)],seg[u]);
@@ -39,6 +36,7 @@ struct Seg{
     node query(int l,int r){
         node vl,vr,aux;
         vl.off = vr.off = true;
+        if(l > r) return vl;
         for(l+=n,r+=n+1;l<r;l>>=1,r>>=1){
             if (l&1) merge(aux=vl,seg[l++],vl);
             if (r&1) merge(seg[--r],aux=vr,vr);
@@ -46,8 +44,7 @@ struct Seg{
         merge(vl,vr,aux); return aux;
     }
     void update(int id,upd x){
-        id += n;
-        apply(seg[id],x); 
+        id += n; apply(seg[id],x); 
         for(id>>=1; id; id>>=1) merge(seg[lef(id)],seg[rig(id)],seg[id]);
     }
     #undef lef
