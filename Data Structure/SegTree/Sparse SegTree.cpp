92a59d87b47aed60aba4b72de570a17e31fdb0d1
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-04 17:59:49 -0300

    Update Sparse SegTree.cpp

--- Data Structure/SegTree/Sparse SegTree.cpp
+++ Data Structure/SegTree/Sparse SegTree.cpp
@@ -2,16 +2,20 @@
 // Query com operacao associativa e update em range
 // Query(4*log(N)*O(merge)), Update(4*log(N)*O(merge))
 // Cada update/query adiciona log(N) de memoria
-// Arrumar o construtor do node para dar o valor identidade da operacao (cuidado com o valor off)
+// Inicializar o node com valor identidade
+// Se precisar da range de cada node, lembra de passar true quando inicializar a Seg
 struct node{
 
-    bool off = false;
+    // T tl,tr; // range do node
     node(){}
+    void off(){} // transformar esse node na identidade
     friend void merge(node &x,node &y,node &at){
-        if(x.off) return void(at = y);
-        if(y.off) return void(at = x);
         // o at eh o merge do x(esq) e y(dir)
-        
+
+        // se for usar range do node
+        // if(x.tl == -1) at.tl = y.tl, at.tr = y.tr;
+        // else if(y.tl == -1) at.tl = x.tl, at.tr = x.tr;
+        // else at.tl = x.tl, at.tr = y.tr;
     }
 };
 struct upd{
@@ -32,10 +36,10 @@ struct Seg{
     #define rig(x) prox[x][1]
     #define msb(x) ((x) == 0 ? -1 : __builtin_clzll(1ll) - __builtin_clzll(x))
     T n; vc<node> seg; vc<upd> lazy; vc<array<int,2>> prox;
-    node ret,aux;
-    Seg(T nn = 1, int q = 0) : n(nn){ // passar qtd de Queries em q
+    node ret,aux; bool flag;
+    Seg(T nn = 1,bool range=false,int q = 0) : n(nn), flag(range){ // passar qtd de Queries em q
         if(q > 0) seg.reserve(2*q*(msb(n)+1));
-        add();
+        add(); if(flag) seg[0].tl = 0, seg[0].tr = n-1;
     }
     int add(){
         int x = seg.size();
@@ -45,6 +49,12 @@ struct Seg{
     void add(int u){
         if(lef(u)) return;
         lef(u) = add(), rig(u) = add();
+        if(flag){
+            T tl = seg[u].tl, tr = seg[u].tr;
+            T tmid = tl+tr; tmid >>= 1;
+            seg[lef(u)].tl = tl, seg[lef(u)].tr = tmid;
+            seg[rig(u)].tl = tmid+1, seg[rig(u)].tr = tr;
+        }
     }
     void push(int u,T tl,T tr){
         if(tl == tr || lazy[u].off) return;
@@ -59,8 +69,8 @@ struct Seg{
         query(lef(u),tl,tmid,l,min(r,tmid)),query(rig(u),tmid+1,tr,max(l,tmid+1),r);
     }
     node query(T l, T r){
-        ret.off = true; query(0,0,n-1,l,r);
-        return ret; // vc que trata quando o ret.off == true
+        ret.off(); query(0,0,n-1,l,r);
+        return ret;
     }
     void update(int u,T tl,T tr,T l, T r, upd& x){
         if(l > r) return;
