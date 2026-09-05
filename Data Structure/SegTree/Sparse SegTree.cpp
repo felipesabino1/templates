45c8f1926d7e47d9b783a13b175912e04e116875
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-05 17:02:49 -0300

    Update Sparse SegTree.cpp

--- Data Structure/SegTree/Sparse SegTree.cpp
+++ Data Structure/SegTree/Sparse SegTree.cpp
@@ -25,7 +25,7 @@ template <class node, class upd, class T>
 struct Seg{
     #define lef(x) prox[x][0]
     #define rig(x) prox[x][1]
-    #define check(x) x = x == -1 ? add() : x;
+    #define check(x) x = x == -1 ? add() : x
     T n; vc<node> seg; vc<array<int,2>> prox;
     node ret,aux;
     Seg(T nn = 1,int q = 0) : n(nn){ // passar qtd de Queries em q
@@ -40,34 +40,25 @@ struct Seg{
         seg.emplace_back(),prox.push_back({-1,-1});
         return x;
     }
-    int query(int u,T tl,T tr,T l, T r){
-        if(l > r) return -1;
-        check(u);
-        if(l == tl && tr == r){
-            merge(aux = ret, seg[u], ret);
-            return u;
-        }
+    void query(int u,T tl,T tr,T l, T r){
+        if(l > r) return;
+        if(l == tl && tr == r) return merge(aux = ret, seg[u], ret);
         T tmid = tl+tr; tmid >>= 1;
-        lef(u) = max(lef(u),query(lef(u),tl,tmid,l,min(r,tmid)));
-        rig(u) = max(rig(u),query(rig(u),tmid+1,tr,max(l,tmid+1),r));
-        return u;
+        check(lef(u)), check(rig(u));
+        query(lef(u),tl,tmid,l,min(r,tmid)),query(rig(u),tmid+1,tr,max(l,tmid+1),r);
     }
     node query(T l, T r){
         assert(0 <= l && l <= r && r < n);
         ret.off(); query(0,0,n-1,l,r);
         return ret;
     }
-    int update(int u,T tl,T tr,T id, upd& x){
-        check(u);
-        if(tl == tr){
-            apply(seg[u],x);
-            return u;
-        }
+    void update(int u,T tl,T tr,T id, upd& x){
+        if(tl == tr) return apply(seg[u],x);
         T tmid = tl+tr; tmid >>= 1;
-        if(tmid >= id) lef(u) = update(lef(u),tl,tmid,id,x);
-        else rig(u) = update(rig(u),tmid+1,tr,id,x);
+        check(lef(u)), check(rig(u));
+        if(tmid >= id) update(lef(u),tl,tmid,id,x);
+        else update(rig(u),tmid+1,tr,id,x);
         merge(seg[lef(u)],seg[rig(u)],seg[u]);
-        return u;
     }
     void update(T id, upd x){assert(0 <= id && id < n); update(0,0,n-1,id,x);} // [l,r]
     #undef lef
