f497b742389930c9ece042f10047db4b18ff3496
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-05 16:02:51 -0300

    Update Sparse SegTree.cpp

--- Data Structure/SegTree/Sparse SegTree.cpp
+++ Data Structure/SegTree/Sparse SegTree.cpp
@@ -37,30 +37,37 @@ struct Seg{
     }
     int add(){
         int x = seg.size();
-        seg.emplace_back(),prox.emplace_back();
+        seg.emplace_back(),prox.emplace_back(-1,-1);
         return x;
     }
-    void add(int u){
-        if(lef(u)) return;
-        lef(u) = add(), rig(u) = add();
-    }
-    void query(int u,T tl,T tr,T l, T r){
-        if(l > r) return;
-        if(l == tl && tr == r) return merge(aux = ret,seg[u],ret);
-        T tmid = tl+tr; tmid >>= 1; add(u);
-        query(lef(u),tl,tmid,l,min(r,tmid)),query(rig(u),tmid+1,tr,max(l,tmid+1),r);
+    int query(int u,T tl,T tr,T l, T r){
+        if(l > r) return -1;
+        u = (u == -1 ? add() : u);
+        if(l == tl && tr == r){
+            merge(aux = ret, seg[u], ret);
+            return u;
+        }
+        T tmid = tl+tr; tmid >>= 1;
+        lef(u) = max(lef(u),query(lef(u),tl,tmid,l,min(r,tmid)));
+        rig(u) = max(rig(u),query(rig(u),tmid+1,tr,max(l,tmid+1),r));
+        return u;
     }
     node query(T l, T r){
         assert(0 <= l && l <= r && r < n);
         ret.off(); query(0,0,n-1,l,r);
         return ret;
     }
-    void update(int u,T tl,T tr,T id, upd& x){
-        if(tl == tr) return apply(seg[u],x);
-        T tmid = tl+tr; tmid >>= 1; add(u);
-        if(tmid >= id) update(lef(u),tl,tmid,id,x);
-        else update(rig(u),tmid+1,tr,id,x);
+    int update(int u,T tl,T tr,T id, upd& x){
+        u = (u == -1 ? add() : u);
+        if(tl == tr){
+            apply(seg[u],x);
+            return u;
+        }
+        T tmid = tl+tr; tmid >>= 1;
+        if(tmid >= id) lef(u) = update(lef(u),tl,tmid,id,x);
+        else rig(u) = update(rig(u),tmid+1,tr,id,x);
         merge(seg[lef(u)],seg[rig(u)],seg[u]);
+        return u;
     }
     void update(T id, upd x){assert(0 <= id && id < n); update(0,0,n-1,id,x);} // [l,r]
     #undef lef
