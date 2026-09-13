cb62c42b087514c6acdd00b0929e986f8efa8848
Felipe Sabino <133066719+felipesabino1@users.noreply.github.com>
2026-09-13 13:32:16 -0300

    Update Persistent SegTree.cpp

--- Data Structure/SegTree/Persistent SegTree.cpp
+++ Data Structure/SegTree/Persistent SegTree.cpp
@@ -1,15 +1,13 @@
 // Indexado de 0
-// Query com op associativa e update em ponto
-// Query e update: 4*log(N)
-// N*log(N) de memoria (cada update adiciona log(N))
-// Definir tudo que ta fora de Seg
+// Query com operacao associativa e update em ponto
+// Query(4*log(N)*O(merge)), Update(4*log(N)*O(merge))
+// Cada update adiciona log(N) de memoria
+// Inicializar o node com valor identidade
 struct node{
     
-    bool off = false;int l = 0,r = 0;
     node(){}
+    void off(){} // transformar esse node na identidade
     friend void merge(node &x,node &y,node &at){
-        if(x.off) return void(at = y);
-        if(y.off) return void(at = x);
         // o at eh o merge do x(esq) e y(dir)
         
     }
@@ -19,29 +17,33 @@ struct upd{
     upd(){}
     // aplica upd
     friend void apply(node &at,upd &x){
-
+        // upd node
     }
 };
 template <class node, class upd>
 struct perSeg{
-    #define lef(x) seg[x].l
-    #define rig(x) seg[x].r
-    perSeg(int nn = 0,vc<node> &v = {}) : n(nn),seg(1),rev(1,0){
+    #define lef(x) prox[x][0]
+    #define rig(x) prox[x][1]
+    #define check(x) x = x == -1 ? add() : x
+    T n; vc<node> seg; vc<array<int,2>> prox; vc<int> rev; // raiz da revisao    
+    node ret,aux;
+    perSeg(int nn = 0,vc<node> v = {}) : n(nn),seg(1),rev(1,0){
         if(!v.empty()) build(0,0,n-1,v);
     }
     void init(vc<node> &v){
         assert(v.size() == n);
         build(0,0,n-1,v);
     }
-    int n; vc<node> seg;
-    vc<int> rev; // raiz da revisao
-    node ret,aux;
+    int add(){
+        int x = seg.size();
+        seg.emplace_back(),prox.push_back({-1,-1});
+        return x;
+    }
     int get_rev(){return int(rev.size())-1;}
     void build(int u,int tl,int tr,vc<node>& v){
         if(tl == tr) return void(seg[u] = v[tl]);
         int tmid = tl + tr; tmid >>= 1;
-        lef(u) = seg.size(); seg.emplace_back();
-        rig(u) = seg.size(); seg.emplace_back();
+        check(lef(u)), check(rig(u));
         build(lef(u),tl,tmid,v),build(rig(u),tmid+1,tr,v);
         merge(seg[lef(u)],seg[rig(u)],seg[u]);
     }
@@ -52,26 +54,24 @@ struct perSeg{
         query(lef(u),tl,tmid,l,min(r,tmid)),query(rig(u),tmid+1,tr,max(l,tmid+1),r);
     }
     node query(int l,int r,int R){
-        ret.off = true; query(rev[R],0,n-1,l,r);
+        assert(0 <= l && l <= r && r < n);
+        ret.off(); query(rev[R],0,n-1,l,r);
         return ret;
     }
     void update(int u,int nu,int tl,int tr,int id,upd &x){
         if(tl == tr) return apply(seg[nu],x);
         int tmid = tl + tr; tmid >>= 1;
         lef(nu) = lef(u), rig(nu) = rig(u);
-        if(id <= tmid){
-            lef(nu) = seg.size(); seg.emplace_back(seg[lef(u)]);
-            update(lef(u),lef(nu),tl,tmid,id,x);
-        }else{
-            rig(nu) = seg.size(); seg.emplace_back(seg[rig(u)]);
-            update(rig(u),rig(nu),tmid+1,tr,id,x);
-        }
+        if(id <= tmid) check(lef(nu)), update(lef(u),lef(nu),tl,tmid,id,x);
+        else check(rig(nu)), update(rig(u),rig(nu),tmid+1,tr,id,x);
         merge(seg[lef(nu)],seg[rig(nu)],seg[nu]);
     }
     void update(int id,upd x,int r){
-        rev.push_back(seg.size()); seg.emplace_back(seg[rev[r]]);
+        assert(0 <= id && id < n);
+        rev.push_back(seg.size()), seg.emplace_back(seg[rev[r]]);
         update(rev[r],rev.back(),0,n-1,id,x);
     }
     #undef lef
     #undef rig
+    #undef check
 };
